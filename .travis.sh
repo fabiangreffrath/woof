85120fe5c8a4f850b7ded563cc223dea48d66972
Fabian Greffrath <fabian@greffrath.com>
2020-01-08 14:14:07 +0100

    add Travis CI

---
+++ .travis.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+if [ "$ANALYZE" = "true" ] ; then
+	cppcheck --error-exitcode=1 -j2 -DMY_SDL_VER -ISource Source 2> stderr.txt
+	RET=$?
+	if [ -s stderr.txt ]
+	then
+		cat stderr.txt
+	fi
+	exit $RET
+else
+	set -e
+	autoreconf -fiv
+	./configure --enable-werror
+	make -j4
+	make install DESTDIR=/tmp/whatever
+	make dist
+fi
