1a589cbf0876a65acb3ed5ca28cad429a4c1c855
Fabian Greffrath <fabian@greffrath.com>
2020-01-14 08:54:47 +0100

    add a comment to the new function prototype

--- Source/v_video.h
+++ Source/v_video.h
@@ -117,6 +117,9 @@ void V_DrawBlock(int x, int y, int scrn, int width, int height, byte *src);
 // Reads a linear block of pixels into the view buffer.
 
 void V_GetBlock(int x, int y, int scrn, int width, int height, byte *dest);
+
+// [FG] non hires-scaling variant of V_DrawBlock, used in disk icon drawing
+
 void V_PutBlock(int x, int y, int scrn, int width, int height, byte *src);
 
 #define V_MarkRect(x,y,width,height)  /* killough 11/98: unused */
