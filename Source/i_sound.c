204ceec0ce7e3ff6ae9f0af3042a4a525ed849b1
Fabian Greffrath <fabian@greffrath.com>
2020-02-16 15:32:42 +0100

    pimp up pitch-shifted sounds a bit

--- Source/i_sound.c
+++ Source/i_sound.c
@@ -383,7 +383,7 @@ void I_SetChannels(void)
    // This table provides step widths for pitch parameters.
    for(i=-128 ; i<128 ; i++)
    {
-      steptablemid[i] = (int)(pow(1.2, (double)i / 64.0) * 65536.0);
+      steptablemid[i] = (int)(pow(2., (double)i / 64.0) * 65536.0);
    }
 }
 
