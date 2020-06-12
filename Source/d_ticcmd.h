9b6388eedc85ceac66b878d0e803a1869b726391
Fabian Greffrath <fabian@greffrath.com>
2020-06-12 08:53:47 +0200

    fix a char signedness issue
    Thanks Baron Pampa!

--- Source/d_ticcmd.h
+++ Source/d_ticcmd.h
@@ -37,8 +37,9 @@
 // plus a checksum for internal state consistency.
 typedef struct
 {
-    char	forwardmove;	// *2048 for move
-    char	sidemove;	// *2048 for move
+    // [FG] signed char!
+    signed char	forwardmove;	// *2048 for move
+    signed char	sidemove;	// *2048 for move
     short	angleturn;	// <<16 for angle delta
     short	consistancy;	// checks for net game
     byte	chatchar;
