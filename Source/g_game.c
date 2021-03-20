b6b7d999f27d070909faca0ecd0de8c83674a877
Fabian Greffrath <fabian@greffrath.com>
2021-03-20 21:23:07 +0100

    only save MUSINFO music if it's actually playing

--- Source/g_game.c
+++ Source/g_game.c
@@ -1521,7 +1521,10 @@ static void G_DoSaveGame(void)
 
   // save lump name for current MUSINFO item
   CheckSaveGame(8);
-  memcpy(save_p, lumpinfo[musinfo.current_item].name, 8);
+  if (musinfo.current_item > 0)
+    memcpy(save_p, lumpinfo[musinfo.current_item].name, 8);
+  else
+    memset(save_p, 0, 8);
   save_p += 8;
 
   length = save_p - savebuffer;
