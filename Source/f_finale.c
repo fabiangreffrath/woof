73bfe2dc48ec3a5225dde5a494668414bf00dc0d
Roman Fomin <rfomin@gmail.com>
2021-04-21 23:20:23 +0700

    fix draw widescreen graphics for Doom 1 endings

--- Source/f_finale.c
+++ Source/f_finale.c
@@ -864,21 +864,21 @@ void F_Drawer (void)
     {
       case 1:
            if ( gamemode == retail )
-             V_DrawPatch (0,0,0,
+             V_DrawPatchFullScreen (0,
                W_CacheLumpName("CREDIT",PU_CACHE));
            else
-             V_DrawPatch (0,0,0,
+             V_DrawPatchFullScreen (0,
                W_CacheLumpName("HELP2",PU_CACHE));
            break;
       case 2:
-           V_DrawPatch(0,0,0,
+           V_DrawPatchFullScreen (0,
              W_CacheLumpName("VICTORY2",PU_CACHE));
            break;
       case 3:
            F_BunnyScroll ();
            break;
       case 4:
-           V_DrawPatch (0,0,0,
+           V_DrawPatchFullScreen (0,
              W_CacheLumpName("ENDPIC",PU_CACHE));
            break;
     }
