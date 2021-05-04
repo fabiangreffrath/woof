caf37cbe4e0dab0ea0abcefb1d91e096607cb574
Fabian Greffrath <fabian@greffrath.com>
2021-05-04 11:51:07 +0200

    move win_opendir.c to the win32 directory

--- Source/i_glob.c
+++ Source/i_glob.c
@@ -27,7 +27,7 @@
 
 #if defined(_MSC_VER)
 // For Visual C++, we need to include the win_opendir module.
-#include "win_opendir.h"
+#include "../win32/win_opendir.h"
 #define strcasecmp _stricmp
 #define strncasecmp _strnicmp
 #include <sys/stat.h>
