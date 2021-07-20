8f49e5cb3f2e69a8f08aaa09a9edf4daa803b7ff
Roman Fomin <rfomin@gmail.com>
2021-07-20 23:04:45 +0700

    fix load game during multiplayer demo playback (AV.WAD demo1)

--- Source/g_game.c
+++ Source/g_game.c
@@ -1819,6 +1819,14 @@ static void G_DoLoadGame(void)
   char vcheck[VERSIONSIZE];
   byte saveg_complevel = MBFVERSION;
 
+  // [crispy] loaded game must always be single player.
+  // Needed for ability to use a further game loading, as well as
+  // cheat codes and other single player only specifics.
+  // if (startloadgame == -1)
+  netdemo = false;
+  netgame = false;
+  deathmatch = false;
+
   gameaction = ga_nothing;
 
   length = M_ReadFile(savename, &savebuffer);
