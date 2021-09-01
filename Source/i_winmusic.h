93a0909e558ddc2893c476735f1c41039101a401
Roman Fomin <rfomin@gmail.com>
2021-09-01 22:51:23 +0700

    add missing copyright

--- Source/i_winmusic.h
+++ Source/i_winmusic.h
@@ -1,3 +1,6 @@
+//
+//  Copyright(C) 2021 Roman Fomin
+//
 //  This program is free software; you can redistribute it and/or
 //  modify it under the terms of the GNU General Public License
 //  as published by the Free Software Foundation; either version 2
