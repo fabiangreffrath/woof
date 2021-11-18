fa6f422cbf7f1df5bd6ae5a87e79926e8b3d9c3a
Roman Fomin <rfomin@gmail.com>
2021-11-18 23:48:39 +0700

    fix gcc warning

--- Source/wi_stuff.c
+++ Source/wi_stuff.c
@@ -1945,7 +1945,7 @@ void WI_DrawBackground(void)
   else if (gamemode == commercial || wbs->epsd >= 3)
     strcpy(name, "INTERPIC");
   else 
-    sprintf(name, "WIMAP%d", wbs->epsd);
+    sprintf(name, "WIMAP%1.1d", wbs->epsd);
 
   // background
   bg = W_CacheLumpName(name, PU_CACHE);    
