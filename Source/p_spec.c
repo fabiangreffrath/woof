a3a4dc529a30c950f5c06056fadadbdc1851f3e4
Roman Fomin <rfomin@gmail.com>
2022-03-30 16:56:34 +0700

    remove declaration

--- Source/p_spec.c
+++ Source/p_spec.c
@@ -2505,10 +2505,8 @@ void P_SpawnSpecials (void)
 
       case 271:   // Regular sky
       case 272:   // Same, only flipped
-        // Texture comes from upper texture of reference sidedef
-        int texture = texturetranslation[sides[*lines[i].sidenum].toptexture];
         // Pre-calculate sky color
-        R_GetSkyColor(texture);
+        R_GetSkyColor(texturetranslation[sides[*lines[i].sidenum].toptexture]);
         for (s = -1; (s = P_FindSectorFromLineTag(lines+i,s)) >= 0;)
           sectors[s].sky = i | PL_SKYFLAT;
         break;
