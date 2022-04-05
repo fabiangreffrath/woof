94fd2f2a61fc8408967b4308ab5409213beb162a
Roman Fomin <rfomin@gmail.com>
2022-04-05 09:13:35 +0700

    simplify warning

--- Source/p_maputl.c
+++ Source/p_maputl.c
@@ -501,11 +501,6 @@ boolean PIT_AddLineIntercepts(line_t *ld)
   intercept_p->isaline = true;
   intercept_p->d.line = ld;
   InterceptsOverrun(intercept_p - intercepts, intercept_p);
-  if (intercept_p - intercepts == MAXINTERCEPTS_ORIGINAL + 1)
-  {
-    // [crispy] print a warning
-    fprintf(stderr, "PIT_AddLineIntercepts: Triggered INTERCEPTS overflow!\n");
-  }
   intercept_p++;
 
   return true;  // continue
@@ -562,11 +557,6 @@ boolean PIT_AddThingIntercepts(mobj_t *thing)
   intercept_p->isaline = false;
   intercept_p->d.thing = thing;
   InterceptsOverrun(intercept_p - intercepts, intercept_p);
-  if (intercept_p - intercepts == MAXINTERCEPTS_ORIGINAL + 1)
-  {
-    // [crispy] print a warning
-    fprintf(stderr, "PIT_AddThingIntercepts: Triggered INTERCEPTS overflow!\n");
-  }
   intercept_p++;
 
   return true;          // keep going
@@ -704,6 +694,12 @@ static void InterceptsOverrun(int num_intercepts, intercept_t *intercept)
         // No overrun
 
         return;
+    }
+
+    if (num_intercepts == MAXINTERCEPTS_ORIGINAL + 1)
+    {
+        // [crispy] print a warning
+        fprintf(stderr, "Triggered INTERCEPTS overflow!\n");
     }
 
     location = (num_intercepts - MAXINTERCEPTS_ORIGINAL - 1) * 12;
