fa81ee18ff57b9b787cdf68a8b53f04ab24f0634
Roman Fomin <rfomin@gmail.com>
2022-06-09 11:35:38 +0700

    another attempt to fix frame interpolation for the newsync method (#577)
    * another attempt to fix frame interpolation for the newsync method
    
    * add uncapped check for safety

--- Source/d_loop.c
+++ Source/d_loop.c
@@ -718,7 +718,20 @@ void TryRunTics (void)
 
     if (new_sync)
     {
-	counts = availabletics;
+        if (uncapped)
+        {
+            // decide how many tics to run
+            if (realtics < availabletics-1)
+                counts = realtics+1;
+            else if (realtics < availabletics)
+                counts = realtics;
+            else
+                counts = availabletics;
+        }
+        else
+        {
+            counts = availabletics;
+        }
 
         // [AM] If we've uncapped the framerate and there are no tics
         //      to run, return early instead of waiting around.
