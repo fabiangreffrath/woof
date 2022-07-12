1b13549f3bf0d7049ecb0d260782d7eb529134d1
Roman Fomin <rfomin@gmail.com>
2022-07-12 13:41:22 +0700

    fix format error

--- src/m_menu.c
+++ src/m_menu.c
@@ -3860,7 +3860,7 @@ static void M_ResetTimeScale(void)
     {
       time_scale = M_ParmArgToInt(p);
       if (time_scale < 10 || time_scale > 1000)
-        I_Error("Invalid parameter '%s' for -speed, valid values are 10-1000.",
+        I_Error("Invalid parameter '%d' for -speed, valid values are 10-1000.",
                 time_scale);
     }
 
