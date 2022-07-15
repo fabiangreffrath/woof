cf52753a1f437366bf3a5ea238650f3fc8b1772a
Roman Fomin <rfomin@gmail.com>
2022-07-15 15:23:51 +0700

    we don't need new macro
    I'm confused about my own macros.

--- src/d_main.c
+++ src/d_main.c
@@ -245,7 +245,7 @@ void D_Display (void)
     }
 
   // save the current screen if about to wipe
-  if ((wipe = gamestate != wipegamestate) && NOTSTRICTMODE(screen_melt))
+  if ((wipe = gamestate != wipegamestate) && !STRICTMODE(screen_melt))
     wipe_StartScreen(0, 0, SCREENWIDTH, SCREENHEIGHT);
 
   if (gamestate == GS_LEVEL && gametic)
