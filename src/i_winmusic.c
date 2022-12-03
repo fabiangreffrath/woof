87abeb982b7782cf64b6556e0205f4c1b1c0b662
ceski <ceski.dx@gmail.com>
2022-12-03 05:25:53 -0800

    Update copyright

--- src/i_winmusic.c
+++ src/i_winmusic.c
@@ -1,5 +1,6 @@
 //
-// Copyright(C) 2021 Roman Fomin
+// Copyright(C) 2021-2022 Roman Fomin
+// Copyright(C) 2022 ceski
 //
 // This program is free software; you can redistribute it and/or
 // modify it under the terms of the GNU General Public License
