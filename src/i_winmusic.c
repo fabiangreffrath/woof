c7e09871dd091c7c535afde59ab7b99a4d79f4ac
Roman Fomin <rfomin@gmail.com>
2022-12-03 23:43:38 +0700

    Revert "winmidi: remove unnecessary `midiOutReset` call"
    Ops, misunderstand comment.
    
    This reverts commit 912b325172756dce19b639a23358cecaeab165fa.

--- src/i_winmusic.c
+++ src/i_winmusic.c
@@ -726,6 +726,11 @@ static void I_WIN_StopSong(void *handle)
     {
         MidiError("midiStreamStop", mmr);
     }
+    mmr = midiOutReset((HMIDIOUT)hMidiStream);
+    if (mmr != MMSYSERR_NOERROR)
+    {
+        MidiError("midiOutReset", mmr);
+    }
 }
 
 static void I_WIN_PlaySong(void *handle, boolean looping)
