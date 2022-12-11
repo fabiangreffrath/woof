47c865a310b4e703c2f5e975d16212af1b360099
Roman Fomin <rfomin@gmail.com>
2022-12-11 14:02:45 +0700

    reduce verbosity
    `printf()` does not work with unicode on Windows without complications

--- src/i_flmusic.c
+++ src/i_flmusic.c
@@ -271,9 +271,6 @@ static boolean I_FL_InitMusic(int device)
         return false;
     }
 
-    printf("FluidSynth Init: Using %s.\n",
-        lumpnum >= 0 ? "SNDFONT lump" : soundfont_path);
-
     return true;
 }
 
