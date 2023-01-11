cde9dd6aeca1c1d91bd6ddee550714d9e8055ee7
Roman Fomin <rfomin@gmail.com>
2023-01-11 14:54:28 +0700

    add custom triplets for vcpkg
    * Build only release packages.
    
    * Use the "/fp:fast" compiler option for libsamplerate, which greatly increases
      speed (up to 10x for SRC_LINEAR).

---
+++ triplets-custom/x64-windows.cmake
@@ -0,0 +1,9 @@
+set(VCPKG_TARGET_ARCHITECTURE x64)
+set(VCPKG_CRT_LINKAGE dynamic)
+set(VCPKG_LIBRARY_LINKAGE dynamic)
+set(VCPKG_BUILD_TYPE release)
+
+if(${PORT} MATCHES "libsamplerate")
+    set(VCPKG_CXX_FLAGS "/fp:fast")
+    set(VCPKG_C_FLAGS "/fp:fast")
+endif()
