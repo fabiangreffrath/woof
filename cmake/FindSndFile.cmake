03a76fb2389b0313b943a59ae981c109c493b5eb
Roman Fomin <rfomin@gmail.com>
2023-04-05 03:15:29 +0700

    more FindSndFile.cmake fixes, formatting

--- cmake/FindSndFile.cmake
+++ cmake/FindSndFile.cmake
@@ -6,43 +6,38 @@
 # Environment variables used:
 #  SNDFILE_ROOT
 
-if(SndFile_INCLUDE_DIR)
-  # Already in cache, be silent
-  set(SndFile_FIND_QUIETLY TRUE)
-endif(SndFile_INCLUDE_DIR)
-
 find_package(PkgConfig QUIET)
 pkg_check_modules(PC_SndFile QUIET sndfile)
 
 set(SndFile_VERSION ${PC_SndFile_VERSION})
 
-find_path(SndFile_INCLUDE_DIR sndfile.h HINTS
+find_path(SndFile_INCLUDE_DIR sndfile.h
+    HINTS
     ${PC_SndFile_INCLUDEDIR}
     ${PC_SndFile_INCLUDE_DIRS}
     ${SndFile_ROOT})
 
 find_library(SndFile_LIBRARY NAMES sndfile
-  HINTS
+    HINTS
     ${PC_SndFile_LIBDIR}
     ${PC_SndFile_LIBRARY_DIRS}
     ${SndFile_ROOT})
 
 include(FindPackageHandleStandardArgs)
 find_package_handle_standard_args(SndFile
-  REQUIRED_VARS
+    REQUIRED_VARS
     SndFile_LIBRARY
     SndFile_INCLUDE_DIR
-  VERSION_VAR
+    VERSION_VAR
     SndFile_VERSION)
 
 if(SndFile_FOUND)
-  if(NOT TARGET SndFile::sndfile)
-    add_library(SndFile::sndfile UNKNOWN IMPORTED)
-    set_target_properties(SndFile::sndfile PROPERTIES
-      INTERFACE_INCLUDE_DIRECTORIES "${SndFile_INCLUDE_DIR}"
-      IMPORTED_LOCATION "${SndFile_LIBRARY}"
-      INTERFACE_LINK_LIBRARIES "Vorbis::vorbisenc;Opus::opus;FLAC::FLAC")
-  endif()
+    if(NOT TARGET SndFile::sndfile)
+      add_library(SndFile::sndfile UNKNOWN IMPORTED)
+      set_target_properties(SndFile::sndfile PROPERTIES
+          INTERFACE_INCLUDE_DIRECTORIES "${SndFile_INCLUDE_DIR}"
+          IMPORTED_LOCATION "${SndFile_LIBRARY}")
+    endif()
 endif()
 
 mark_as_advanced(SndFile_LIBRARY SndFile_INCLUDE_DIR)
