b4a5b599f67aad30ccd12db9d0ff007d3cb84d53
Roman Fomin <rfomin@gmail.com>
2023-04-20 16:02:47 +0700

    switch to libxmp from libmodplug

---
+++ src/i_xmp.c
@@ -0,0 +1,132 @@
+//
+// Copyright(C) 2023 Roman Fomin
+//
+// This program is free software; you can redistribute it and/or
+// modify it under the terms of the GNU General Public License
+// as published by the Free Software Foundation; either version 2
+// of the License, or (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// DESCRIPTION:
+//
+
+#ifdef HAVE_LIBXMP
+
+#include <xmp.h>
+
+#include "doomtype.h"
+#include "i_sound.h"
+#include "i_oalmusic.h"
+
+static xmp_context context;
+
+static void PrintError(int e)
+{
+    const char *msg;
+    switch (e)
+    {
+        case -XMP_ERROR_INTERNAL:
+            msg = "Internal error in libxmp.";
+            break;
+        case -XMP_ERROR_FORMAT:
+            msg = "Unrecognized file format.";
+            break;
+        case -XMP_ERROR_LOAD:
+            msg = "Error loading file.";
+            break;
+        case -XMP_ERROR_DEPACK:
+            msg = "Error depacking file.";
+            break;
+        case -XMP_ERROR_SYSTEM:
+            msg = "System error in libxmp.";
+            break;
+        case -XMP_ERROR_INVALID:
+            msg = "Invalid parameter.";
+            break;
+        case -XMP_ERROR_STATE:
+            msg = "Invalid player state.";
+            break;
+        default:
+            msg = "Unknown error.";
+            break;
+    }
+    printf("XMP: %s\n", msg);
+}
+
+static boolean I_XMP_OpenStream(void *data, ALsizei size, ALenum *format,
+                                ALsizei *freq, ALsizei *frame_size)
+{
+    int err = 0;
+
+    context = xmp_create_context();
+    if (!context)
+    {
+        printf("XMP: Failed to create context.\n");
+        return false;
+    }
+
+    err = xmp_load_module_from_memory(context, data, (long)size);
+    if (err < 0)
+    {
+        PrintError(err);
+        xmp_free_context(context);
+        return false;
+    }
+
+    err = xmp_start_player(context, SND_SAMPLERATE, 0);
+    if (err < 0)
+    {
+        PrintError(err);
+        xmp_release_module(context);
+        xmp_free_context(context);
+        return false;
+    }
+
+    *format = AL_FORMAT_STEREO16;
+    *freq = SND_SAMPLERATE;
+    *frame_size = 2 * sizeof(short);
+
+    return true;
+}
+
+static uint32_t I_XMP_FillStream(byte *buffer, uint32_t buffer_samples)
+{
+    int ret = xmp_play_buffer(context, buffer, buffer_samples * 4, 0);
+
+    if (ret < 0)
+    {
+        return 0;
+    }
+
+    return buffer_samples;
+}
+
+static void I_XMP_RestartStream(void)
+{
+    xmp_restart_module(context);
+}
+
+static void I_XMP_CloseStream(void)
+{
+    if (context)
+    {
+        xmp_stop_module(context);
+        xmp_end_player(context);
+        xmp_release_module(context);
+        xmp_free_context(context);
+    }
+}
+
+stream_module_t stream_xmp_module =
+{
+    I_XMP_OpenStream,
+    I_XMP_FillStream,
+    I_XMP_RestartStream,
+    I_XMP_CloseStream,
+};
+
+#endif // HAVE_LIBXMP
