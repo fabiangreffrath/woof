52ffd04d27e22e838a654f195570b2c029a80a08
Roman Fomin <rfomin@gmail.com>
2023-04-25 18:13:14 +0700

    disable exclusive fullscreen by default

--- src/m_misc.c
+++ src/m_misc.c
@@ -157,7 +157,7 @@ default_t defaults[] = {
   {
     "exclusive_fullscreen",
     (config_t *) &exclusive_fullscreen, NULL,
-    {1}, {0, 1}, number, ss_none, wad_no,
+    {0}, {0, 1}, number, ss_none, wad_no,
     "1 to enable exclusive fullscreen mode"
   },
 
