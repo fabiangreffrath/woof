8c33950982ff14fe5562c13ac90ac827b23c3ef7
Roman Fomin <rfomin@gmail.com>
2023-05-02 13:17:35 +0700

    vcpkg: fix sndfile FluidSynth option
    * Ignore `C:/Strawberry/perl` PATH in Windows-2022 image. Turns out it contains a broken pkg-config.
    
    * Remove PkgConfigHelper.
    
    * Restore "*.sf3" soundfonts loading.

--- cmake/FindFluidSynth.cmake
+++ cmake/FindFluidSynth.cmake
@@ -31,8 +31,6 @@ The following cache variables may also be set:
 
 ``FluidSynth_INCLUDE_DIR``
   The directory containing ``FluidSynth.h``.
-``FluidSynth_DLL``
-  The path to the FluidSynth Windows runtime.
 ``FluidSynth_LIBRARY``
   The path to the FluidSynth library.
 
@@ -47,64 +45,27 @@ find_path(
   HINTS "${PC_FLUIDSYNTH_INCLUDEDIR}"
 )
 
-find_file(
-  FluidSynth_DLL
-  NAMES fluidsynth.dll libfluidsynth.dll libfluidsynth-3.dll
-  PATH_SUFFIXES bin
-  HINTS "${PC_FLUIDSYNTH_PREFIX}"
-)
-
 find_library(
   FluidSynth_LIBRARY
   NAMES fluidsynth libfluidsynth
   HINTS "${PC_FLUIDSYNTH_LIBDIR}"
 )
 
-if(FluidSynth_DLL OR FluidSynth_LIBRARY MATCHES ".so|.dylib")
-  set(_fluidsynth_library_type SHARED)
-else()
-  set(_fluidsynth_library_type STATIC)
-endif()
-
-get_flags_from_pkg_config("${_fluidsynth_library_type}" "PC_FLUIDSYNTH" "_fluidsynth")
-
 include(FindPackageHandleStandardArgs)
 find_package_handle_standard_args(
   FluidSynth
-  REQUIRED_VARS "FluidSynth_LIBRARY" "FluidSynth_INCLUDE_DIR")
+  REQUIRED_VARS "FluidSynth_LIBRARY" "FluidSynth_INCLUDE_DIR"
+  VERSION_VAR "FluidSynth_VERSION")
 
 if(FluidSynth_FOUND)
   if(NOT TARGET FluidSynth::libfluidsynth)
-    add_library(FluidSynth::libfluidsynth ${_fluidsynth_library_type} IMPORTED)
-    set_target_properties(
-      FluidSynth::libfluidsynth
-      PROPERTIES INTERFACE_INCLUDE_DIRECTORIES "${FluidSynth_INCLUDE_DIR}"
-                 INTERFACE_COMPILE_OPTIONS "${_fluidsynth_compile_options}"
-                 INTERFACE_LINK_LIBRARIES "${_fluidsynth_link_libraries}"
-                 INTERFACE_LINK_DIRECTORIES "${_fluidsynth_link_directories}"
-                 INTERFACE_LINK_OPTIONS "${_fluidsynth_link_options}"
-    )
-  endif()
-
-  if(FluidSynth_DLL)
-    set_target_properties(
-      FluidSynth::libfluidsynth
-      PROPERTIES IMPORTED_LOCATION "${FluidSynth_DLL}"
-                 IMPORTED_IMPLIB "${FluidSynth_LIBRARY}"
-    )
-  else()
+    add_library(FluidSynth::libfluidsynth UNKNOWN IMPORTED)
     set_target_properties(
       FluidSynth::libfluidsynth
       PROPERTIES IMPORTED_LOCATION "${FluidSynth_LIBRARY}"
+                 INTERFACE_INCLUDE_DIRECTORIES "${FluidSynth_INCLUDE_DIR}"
     )
   endif()
-
-  set(FluidSynth_LIBRARIES FluidSynth::libfluidsynth)
-  set(FluidSynth_INCLUDE_DIRS "${FluidSynth_INCLUDE_DIR}")
 endif()
 
-mark_as_advanced(
-  FluidSynth_INCLUDE_DIR
-  FluidSynth_DLL
-  FluidSynth_LIBRARY
-)
+mark_as_advanced(FluidSynth_INCLUDE_DIR FluidSynth_LIBRARY)
--- cmake/FindSndFile.cmake
+++ cmake/FindSndFile.cmake
@@ -9,18 +9,11 @@
 find_package(PkgConfig QUIET)
 pkg_check_modules(PC_SndFile QUIET sndfile)
 
-set(SndFile_VERSION ${PC_SndFile_VERSION})
-
 find_path(SndFile_INCLUDE_DIR sndfile.h
     HINTS
     ${PC_SndFile_INCLUDEDIR}
     ${PC_SndFile_INCLUDE_DIRS}
     ${SNDFILE_DIR})
-
-find_file(SndFile_DLL
-    NAMES sndfile.dll libsndfile.dll libsndfile-1.dll
-    PATH_SUFFIXES bin
-    HINTS ${PC_SndFile_PREFIX})
 
 find_library(SndFile_LIBRARY
     NAMES sndfile libsndfile
@@ -28,14 +21,6 @@ find_library(SndFile_LIBRARY
     ${PC_SndFile_LIBDIR}
     ${PC_SndFile_LIBRARY_DIRS}
     ${SNDFILE_DIR})
-
-if(SndFile_DLL OR SndFile_LIBRARY MATCHES ".so|.dylib")
-    set(_sndfile_library_type SHARED)
-else()
-    set(_sndfile_library_type STATIC)
-endif()
-
-get_flags_from_pkg_config("${_sndfile_library_type}" "PC_SndFile" "_sndfile")
 
 include(FindPackageHandleStandardArgs)
 find_package_handle_standard_args(SndFile
@@ -47,29 +32,12 @@ find_package_handle_standard_args(SndFile
 
 if(SndFile_FOUND)
     if(NOT TARGET SndFile::sndfile)
-        add_library(SndFile::sndfile ${_sndfile_library_type} IMPORTED)
+        add_library(SndFile::sndfile UNKNOWN IMPORTED)
         set_target_properties(SndFile::sndfile
             PROPERTIES
-            INTERFACE_INCLUDE_DIRECTORIES "${SndFile_INCLUDE_DIR}"
-            INTERFACE_COMPILE_OPTIONS "${_sndfile_compile_options}"
-            INTERFACE_LINK_LIBRARIES "${_sndfile_link_libraries}"
-            INTERFACE_LINK_DIRECTORIES "${_sndfile_link_directories}"
-            INTERFACE_LINK_OPTIONS "${_sndfile_link_options}")
+            IMPORTED_LOCATION "${SndFile_LIBRARY}"
+            INTERFACE_INCLUDE_DIRECTORIES "${SndFile_INCLUDE_DIR}")
     endif()
-
-    if(SndFile_DLL)
-        set_target_properties(SndFile::sndfile
-            PROPERTIES
-            IMPORTED_LOCATION "${SndFile_DLL}"
-            IMPORTED_IMPLIB "${SndFile_LIBRARY}")
-    else()
-      set_target_properties(SndFile::sndfile
-            PROPERTIES
-            IMPORTED_LOCATION "${SndFile_LIBRARY}")
-    endif()
-
-    set(SndFile_LIBRARIES SndFile::sndfile)
-    set(SndFile_INCLUDE_DIRS "${SndFile_INCLUDE_DIR}")
 endif()
 
-mark_as_advanced(SndFile_LIBRARY SndFile_INCLUDE_DIR SndFile_DLL)
+mark_as_advanced(SndFile_LIBRARY SndFile_INCLUDE_DIR)
