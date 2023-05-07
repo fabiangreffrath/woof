120880622d5e15a629ca0e1534adb7e10d6adc84
Roman Fomin <rfomin@gmail.com>
2023-05-07 18:50:47 +0700

    edit help string

--- src/m_menu.c
+++ src/m_menu.c
@@ -2627,7 +2627,7 @@ void M_DrawInstructions()
   { // killough 11/98: reformatted
     const char *s = "";
     int color = CR_HILITE,x = setup_select ? color = CR_SELECT,
-      flags & S_INPUT  ? (s = "Press key or button for this action", 49)     :
+      flags & S_INPUT  ? (s = "Press key or button to bind/unbind this action", 22)     :
       flags & S_YESNO  ? (s = "Press ENTER key to toggle", 78)               :
       // [FG] selection of choices
       flags & (S_CHOICE|S_CRITEM|S_THERMO)
