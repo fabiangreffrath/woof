fabiangreffrath/woof

commit 1a278d6105c8df9007f47cda61b6c02a2afc66b6

switch to OpenAl for sound mixing (#967)
* switch to OpeanAl for sound mixing

* Rewrote Load_SNDFile() to use float format. Made libsndfile mandatory.

* update main.yml workflow

* implement OpeanAl music streaming

* remove i_sdlmusic.c and SDL_Mixer dependecy

* enable mpeg support in libsndfile

* require SndFile 1.1.0

* check SF_FORMAT_MPEG_LAYER_III symbol

* use AL_GAIN for opl_gain

opl/opl_sdl.c
@@ -21,11 +21,10 @@
 #include <stdlib.h>
 #include <string.h>
 #include <errno.h>
-#include <assert.h>
 #include <limits.h>
 
 #include "SDL.h"
-#include "SDL_mixer.h"
+#include "i_oalmusic.h"
 
 #include "opl3.h"
 
@@ -75,10 +74,6 @@ static uint64_t pause_offset;
 static opl3_chip opl_chip;
 static int opl_opl3mode;
 
-// Temporary mixing buffer used by the mixing callback.
-
-static uint8_t *mix_buffer = NULL;
-
 // Register number that was written.
 
 static int register_num = 0;
@@ -88,19 +83,7 @@ static int register_num = 0;
 static opl_timer_t timer1 = { 12500, 0, 0, 0 };
 static opl_timer_t timer2 = { 3125, 0, 0, 0 };
 
-// SDL parameters.
-
-static int sdl_was_initialized = 0;
 static int mixing_freq, mixing_channels;
-static Uint16 mixing_format;
-
-static int SDLIsInitialized(void)
-{
-    int freq, channels;
-    Uint16 format;
-
-    return Mix_QuerySpec(&freq, &format, &channels);
-}
 
 // Advance time by the specified number of samples, invoking any
 // callback functions as appropriate.
@@ -155,59 +138,19 @@ static void AdvanceTime(unsigned int nsamples)
     SDL_UnlockMutex(callback_queue_mutex);
 }
 
-#if SDL_BYTEORDER == SDL_LIL_ENDIAN
-  #define OPL_SHORT SDL_SwapLE16
-#else
-  #define OPL_SHORT SDL_SwapBE16
-#endif
-
-int opl_gain = 200;
-
-static void MixAudioFormat(Uint8 *dst, const Uint8 *src, Uint32 len)
-{
-    Sint16 src1, src2;
-    int dst_sample;
-
-    while (len--)
-    {
-        src1 = OPL_SHORT(*(Sint16 *)src);
-        src2 = OPL_SHORT(*(Sint16 *)dst);
-        src += 2;
-        dst_sample = src1 * opl_gain / 100 + src2;
-        if (dst_sample > SHRT_MAX)
-        {
-            dst_sample = SHRT_MAX;
-        }
-        else if (dst_sample < SHRT_MIN)
-        {
-            dst_sample = SHRT_MIN;
-        }
-        *(Sint16 *)dst = OPL_SHORT(dst_sample);
-        dst += 2;
-    }
-}
-
 // Call the OPL emulator code to fill the specified buffer.
 
 static void FillBuffer(uint8_t *buffer, unsigned int nsamples)
 {
-    // This seems like a reasonable assumption.  mix_buffer is
-    // 1 second long, which should always be much longer than the
-    // SDL mix buffer.
-    assert(nsamples < mixing_freq);
-
-    // OPL output is generated into temporary buffer and then mixed
-    // (to avoid overflows etc.)
-    OPL3_GenerateStream(&opl_chip, (Bit16s *) mix_buffer, nsamples);
-    MixAudioFormat(buffer, mix_buffer, nsamples * 2);
+    OPL3_GenerateStream(&opl_chip, (Bit16s *) buffer, nsamples);
 }
 
 // Callback function to fill a new sound buffer:
 
-static void OPL_Mix_Callback(void *udata, Uint8 *stream, int len)
+static int OPL_Callback(byte *stream, int len)
 {
     unsigned int filled, buffer_samples;
-    Uint8 *buffer = (Uint8*)stream;
+    uint8_t *buffer = stream;
 
     // Repeatedly call the OPL emulator update function until the buffer is
     // full.
@@ -253,20 +196,14 @@ static void OPL_Mix_Callback(void *udata, Uint8 *stream, int len)
 
         AdvanceTime(nsamples);
     }
+    return len;
 }
 
 static void OPL_SDL_Shutdown(void)
 {
-    Mix_HookMusic(NULL, NULL);
+    I_OAL_HookMusic(NULL);
 
-    if (sdl_was_initialized)
-    {
-        Mix_CloseAudio();
-        SDL_QuitSubSystem(SDL_INIT_AUDIO);
-        OPL_Queue_Destroy(callback_queue);
-        free(mix_buffer);
-        sdl_was_initialized = 0;
-    }
+    OPL_Queue_Destroy(callback_queue);
 
 /*
     if (opl_chip != NULL)
@@ -289,63 +226,10 @@ static void OPL_SDL_Shutdown(void)
     }
 }
 
-static unsigned int GetSliceSize(void)
-{
-    int limit;
-    int n;
-
-    limit = (opl_sample_rate * MAX_SOUND_SLICE_TIME) / 1000;
-
-    // Try all powers of two, not exceeding the limit.
-
-    for (n=0;; ++n)
-    {
-        // 2^n <= limit < 2^n+1 ?
-
-        if ((1 << (n + 1)) > limit)
-        {
-            return (1 << n);
-        }
-    }
-
-    // Should never happen?
-
-    return 1024;
-}
+int opl_gain = 200;
 
 static int OPL_SDL_Init(unsigned int port_base)
 {
-    // Check if SDL_mixer has been opened already
-    // If not, we must initialize it now
-
-    if (!SDLIsInitialized())
-    {
-        if (SDL_Init(SDL_INIT_AUDIO) < 0)
-        {
-            fprintf(stderr, "Unable to set up sound.\n");
-            return 0;
-        }
-
-        if (Mix_OpenAudioDevice(opl_sample_rate, AUDIO_S16SYS, 2, GetSliceSize(), NULL, SDL_AUDIO_ALLOW_FREQUENCY_CHANGE) < 0)
-        {
-            fprintf(stderr, "Error initialising SDL_mixer: %s\n", Mix_GetError());
-
-            SDL_QuitSubSystem(SDL_INIT_AUDIO);
-            return 0;
-        }
-
-        SDL_PauseAudio(0);
-
-        // When this module shuts down, it has the responsibility to 
-        // shut down SDL.
-
-        sdl_was_initialized = 1;
-    }
-    else
-    {
-        sdl_was_initialized = 0;
-    }
-
     opl_sdl_paused = 0;
     pause_offset = 0;
 
@@ -356,22 +240,9 @@ static int OPL_SDL_Init(unsigned int port_base)
 
     // Get the mixer frequency, format and number of channels.
 
-    Mix_QuerySpec(&mixing_freq, &mixing_format, &mixing_channels);
-
     // Only supports AUDIO_S16SYS
-
-    if (mixing_format != AUDIO_S16SYS || mixing_channels != 2)
-    {
-        fprintf(stderr, 
-                "OPL_SDL only supports native signed 16-bit LSB, "
-                "stereo format!\n");
-
-        OPL_SDL_Shutdown();
-        return 0;
-    }
-
-    // Mix buffer: four bytes per sample (16 bits * 2 channels):
-    mix_buffer = malloc(mixing_freq * 4);
+    mixing_channels = 2;
+    mixing_freq = opl_sample_rate;
 
     // Create the emulator structure:
 
@@ -381,7 +252,8 @@ static int OPL_SDL_Init(unsigned int port_base)
     callback_mutex = SDL_CreateMutex();
     callback_queue_mutex = SDL_CreateMutex();
 
-    Mix_HookMusic(OPL_Mix_Callback, NULL);
+    I_OAL_HookMusic(OPL_Callback);
+    I_OAL_SetGain((float)opl_gain / 100.0f);
 
     return 1;
 }

src/i_oalmusic.c
@@ -1,0 +1,320 @@
+//
+// Copyright(C) 2023 Roman Fomin
+//
+// This program is free software; you can redistribute it and/or
+// modify it under the terms of the GNU General Public License
+// as published by the Free Software Foundation; either version 2
+// of the License, or (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// DESCRIPTION:
+//
+
+#include "i_oalmusic.h"
+
+#include <AL/al.h>
+#include <AL/alext.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include "SDL.h"
+
+#include "doomtype.h"
+#include "i_sndfile.h"
+#include "i_sound.h"
+
+// Define the number of buffers and buffer size (in milliseconds) to use. 4
+// buffers with 8192 samples each gives a nice per-chunk size, and lets the
+// queue last for almost one second at 44.1khz.
+#define NUM_BUFFERS 4
+#define BUFFER_SAMPLES 8192
+
+typedef struct
+{
+    // These are the buffers and source to play out through OpenAL with
+    ALuint buffers[NUM_BUFFERS];
+    ALuint source;
+
+    byte *data;
+
+    // The format of the output stream
+    ALenum format;
+    ALsizei freq;
+} stream_player_t;
+
+static stream_player_t player;
+
+static SDL_Thread *player_thread_handle;
+static int player_thread_running;
+
+static callback_func_t callback;
+
+static boolean UpdatePlayer(void)
+{
+    ALint processed, state;
+
+    // Get relevant source info
+    alGetSourcei(player.source, AL_SOURCE_STATE, &state);
+    alGetSourcei(player.source, AL_BUFFERS_PROCESSED, &processed);
+    if (alGetError() != AL_NO_ERROR)
+    {
+        fprintf(stderr, "UpdatePlayer: Error checking source state\n");
+        return false;
+    }
+
+    // Unqueue and handle each processed buffer
+    while (processed > 0)
+    {
+        ALuint bufid;
+        ALsizei size;
+
+        alSourceUnqueueBuffers(player.source, 1, &bufid);
+        processed--;
+
+        // Read the next chunk of data, refill the buffer, and queue it back on
+        // the source.
+        if (callback)
+        {
+            size = callback(player.data, BUFFER_SAMPLES);
+        }
+        else
+        {
+            size = I_SND_FillStream(player.data, BUFFER_SAMPLES);
+        }
+        if (size > 0)
+        {
+            alBufferData(bufid, player.format, player.data, size, player.freq);
+            alSourceQueueBuffers(player.source, 1, &bufid);
+        }
+        if (alGetError() != AL_NO_ERROR)
+        {
+            fprintf(stderr, "UpdatePlayer: Error buffering data\n");
+            return false;
+        }
+    }
+
+    // Make sure the source hasn't underrun
+    if (state != AL_PLAYING && state != AL_PAUSED)
+    {
+        ALint queued;
+
+        // If no buffers are queued, playback is finished
+        alGetSourcei(player.source, AL_BUFFERS_QUEUED, &queued);
+        if (queued == 0)
+        {
+            return false;
+        }
+
+        alSourcePlay(player.source);
+        if (alGetError() != AL_NO_ERROR)
+        {
+            fprintf(stderr, "UpdatePlayer: Error restarting playback\n");
+            return false;
+        }
+    }
+
+    return true;
+}
+
+static boolean StartPlayer(void)
+{
+    int i;
+
+    player.data = malloc(BUFFER_SAMPLES);
+
+    // Rewind the source position and clear the buffer queue.
+    alSourceRewind(player.source);
+    alSourcei(player.source, AL_BUFFER, 0);
+
+    // Fill the buffer queue
+    for (i = 0; i < NUM_BUFFERS; i++)
+    {
+        ALsizei size;
+
+        // Get some data to give it to the buffer
+        if (callback)
+        {
+            size = callback(player.data, BUFFER_SAMPLES);
+        }
+        else
+        {
+            size = I_SND_FillStream(player.data, BUFFER_SAMPLES);
+        }
+
+        if (size < 1)
+            break;
+
+        alBufferData(player.buffers[i], player.format, player.data,
+                     size, player.freq);
+    }
+    if (alGetError() != AL_NO_ERROR)
+    {
+        fprintf(stderr, "StartPlayer: Error buffering for playback.\n");
+        return false;
+    }
+
+    alSourceQueueBuffers(player.source, i, player.buffers);
+
+    return true;
+}
+
+static int PlayerThread(void *unused)
+{
+    SDL_SetThreadPriority(SDL_THREAD_PRIORITY_TIME_CRITICAL);
+
+    while (player_thread_running && UpdatePlayer())
+    {
+        SDL_Delay(1);
+    }
+
+    return 0;
+}
+
+static boolean I_OAL_InitMusic(int device)
+{
+    alGenBuffers(NUM_BUFFERS, player.buffers);
+    alGenSources(1, &player.source);
+
+    // Set parameters so mono sources play out the front-center speaker and
+    // won't distance attenuate.
+    alSource3i(player.source, AL_POSITION, 0, 0, -1);
+    alSourcei(player.source, AL_SOURCE_RELATIVE, AL_TRUE);
+    alSourcei(player.source, AL_ROLLOFF_FACTOR, 0);
+
+    return true;
+}
+
+static void I_OAL_SetMusicVolume(int volume)
+{
+    alSourcef(player.source, AL_GAIN, (ALfloat)volume / 15.0f);
+}
+
+static void I_OAL_PauseSong(void *handle)
+{
+    alSourcePause(player.source);
+}
+
+static void I_OAL_ResumeSong(void *handle)
+{
+    alSourcePlay(player.source);
+}
+
+static void I_OAL_PlaySong(void *handle, boolean looping)
+{
+    if (!callback)
+    {
+        I_SND_SetLooping(looping);
+    }
+
+    alSourcePlay(player.source);
+    if (alGetError() != AL_NO_ERROR)
+    {
+        fprintf(stderr, "I_OAL_PlaySong: Error starting playback.\n");
+        return;
+    }
+
+    player_thread_running = true;
+    player_thread_handle = SDL_CreateThread(PlayerThread, NULL, NULL);
+}
+
+static void I_OAL_StopSong(void *handle)
+{
+    alSourceStop(player.source);
+}
+
+static void I_OAL_UnRegisterSong(void *handle)
+{
+    if (player_thread_running)
+    {
+        player_thread_running = false;
+        SDL_WaitThread(player_thread_handle, NULL);
+    }
+
+    if (!callback)
+    {
+        I_SND_CloseStream();
+    }
+
+    if (player.data)
+    {
+        free(player.data);
+        player.data = NULL;
+    }
+}
+
+static void I_OAL_ShutdownMusic(void)
+{
+    I_OAL_StopSong(NULL);
+    I_OAL_UnRegisterSong(NULL);
+
+    alDeleteSources(1, &player.source);
+    alDeleteBuffers(NUM_BUFFERS, player.buffers);
+    if (alGetError() != AL_NO_ERROR)
+    {
+        fprintf(stderr, "I_OAL_ShutdownMusic: Failed to delete object IDs.\n");
+    }
+
+    memset(&player, 0, sizeof(stream_player_t));
+}
+
+// Prebuffers some audio from the file, and starts playing the source.
+
+static void *I_OAL_RegisterSong(void *data, int len)
+{
+    if (I_SND_OpenStream(data, len, &player.format, &player.freq) == false)
+        return NULL;
+
+    StartPlayer();
+
+    return (void *)1;
+}
+
+static int I_OAL_DeviceList(const char *devices[], int size, int *current_device)
+{
+    *current_device = 0;
+    return 0;
+}
+
+void I_OAL_SetGain(float gain)
+{
+    alSourcef(player.source, AL_MAX_GAIN, 10.0f);
+    alSourcef(player.source, AL_GAIN, (ALfloat)gain);
+}
+
+void I_OAL_HookMusic(callback_func_t callback_func)
+{
+    if (callback_func)
+    {
+        callback = callback_func;
+
+        player.format = AL_FORMAT_STEREO16;
+        player.freq = snd_samplerate;
+
+        StartPlayer();
+        I_OAL_PlaySong(NULL, false);
+    }
+    else
+    {
+        callback = NULL;
+
+        I_OAL_UnRegisterSong(NULL);
+    }
+}
+
+music_module_t music_oal_module =
+{
+    I_OAL_InitMusic,
+    I_OAL_ShutdownMusic,
+    I_OAL_SetMusicVolume,
+    I_OAL_PauseSong,
+    I_OAL_ResumeSong,
+    I_OAL_RegisterSong,
+    I_OAL_PlaySong,
+    I_OAL_StopSong,
+    I_OAL_UnRegisterSong,
+    I_OAL_DeviceList,
+};

src/i_oalmusic.h
@@ -1,0 +1,27 @@
+//
+// Copyright(C) 2023 Roman Fomin
+//
+// This program is free software; you can redistribute it and/or
+// modify it under the terms of the GNU General Public License
+// as published by the Free Software Foundation; either version 2
+// of the License, or (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// DESCRIPTION:
+//
+
+#ifndef __I_OALMUSIC__
+#define __I_OALMUSIC__
+
+#include "doomtype.h"
+
+typedef int (*callback_func_t)(byte *data, int size);
+
+void I_OAL_HookMusic(callback_func_t callback_func);
+void I_OAL_SetGain(float gain);
+
+#endif

.github/workflows/main.yml
@@ -18,7 +18,15 @@ jobs:
 
     steps:
       - name: Install dependencies
-        run: sudo apt-get update && sudo apt-get install libfluidsynth-dev libsdl2-dev libsdl2-mixer-dev libsdl2-net-dev ninja-build
+        run: |
+          sudo apt-get update
+          sudo apt-get install \
+            libsdl2-dev \
+            libsdl2-net-dev \
+            libopenal-dev \
+            libsndfile1-dev \
+            libfluidsynth-dev \
+            ninja-build
 
       - uses: actions/checkout@v3
 
@@ -67,8 +75,10 @@ jobs:
             mingw-w64-x86_64-cmake
             mingw-w64-x86_64-ninja
             mingw-w64-x86_64-SDL2
-            mingw-w64-x86_64-SDL2_mixer
             mingw-w64-x86_64-SDL2_net
+            mingw-w64-x86_64-openal
+            mingw-w64-x86_64-libsndfile
+            mingw-w64-x86_64-fluidsynth
 
       - uses: actions/checkout@v3
 

.github/workflows/win_msvc.yml
@@ -5,8 +5,8 @@ on:
     branches: [ master ]
     tags: ['*']
     paths-ignore: ['**.md']
-  pull_request:
-    branches: [ master ]
+  # pull_request:
+  #   branches: [ master ]
 
 env:
   VCPKG_ROOT: C:\vcpkg

CMakeLists.txt
@@ -1,6 +1,7 @@
 include(CheckLibraryExists)
 include(CheckIncludeFile)
 include(CheckSymbolExists)
+include(CheckCSourceCompiles)
 include(ExternalProject)
 
 # Adds the cmake directory to the CMake include path.
@@ -72,14 +73,19 @@ option(CMAKE_FIND_PACKAGE_PREFER_CONFIG
 
 # Library requirements.
 find_package(SDL2 2.0.7 REQUIRED)
-find_package(SDL2_mixer 2.0.2 REQUIRED)
 find_package(SDL2_net REQUIRED)
+find_package(OpenAL REQUIRED)
+find_package(SndFile 1.1.0 REQUIRED)
 
 find_package(FluidSynth 2.2.0)
-find_package(SndFile 1.0.28)
 
 # Python 3
 find_package(Python3 COMPONENTS Interpreter)
+
+check_c_source_compiles(
+"#include <sndfile.h>
+int main(void) {return SF_FORMAT_MPEG_LAYER_III;}"
+HAVE_SNDFILE_MPEG)
 
 configure_file(config.h.in config.h)
 

cmake/FindSDL2_mixer.cmake
@@ -1,123 +1,0 @@
-# FindSDL2_mixer.cmake
-#
-# Copyright (c) 2018, Alex Mayfield <alexmax2742@gmail.com>
-# All rights reserved.
-# 
-# Redistribution and use in source and binary forms, with or without
-# modification, are permitted provided that the following conditions are met:
-#     * Redistributions of source code must retain the above copyright
-#       notice, this list of conditions and the following disclaimer.
-#     * Redistributions in binary form must reproduce the above copyright
-#       notice, this list of conditions and the following disclaimer in the
-#       documentation and/or other materials provided with the distribution.
-#     * Neither the name of the <organization> nor the
-#       names of its contributors may be used to endorse or promote products
-#       derived from this software without specific prior written permission.
-# 
-# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
-# ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
-# WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
-# DISCLAIMED. IN NO EVENT SHALL <COPYRIGHT HOLDER> BE LIABLE FOR ANY
-# DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
-# (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
-# LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
-# ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
-# (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
-# SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
-#
-# Currently works with the following generators:
-# - Unix Makefiles (Linux, MSYS2, Linux MinGW)
-# - Ninja (Linux, MSYS2, Linux MinGW)
-# - Visual Studio
-
-# Cache variable that allows you to point CMake at a directory containing
-# an extracted development library.
-set(SDL2_MIXER_DIR "${SDL2_MIXER_DIR}" CACHE PATH "Location of SDL2_mixer library directory")
-
-# Use pkg-config to find library locations in *NIX environments.
-find_package(PkgConfig QUIET)
-if(PKG_CONFIG_FOUND)
-    pkg_search_module(PC_SDL2_MIXER QUIET SDL2_mixer)
-endif()
-
-# Find the include directory.
-if(CMAKE_SIZEOF_VOID_P STREQUAL 8)
-    find_path(SDL2_MIXER_INCLUDE_DIR "SDL_mixer.h"
-        HINTS
-        "${SDL2_MIXER_DIR}/include"
-        "${SDL2_MIXER_DIR}/include/SDL2"
-        "${SDL2_MIXER_DIR}/x86_64-w64-mingw32/include/SDL2"
-        ${PC_SDL2_MIXER_INCLUDE_DIRS})
-else()
-    find_path(SDL2_MIXER_INCLUDE_DIR "SDL_mixer.h"
-        HINTS
-        "${SDL2_MIXER_DIR}/include"
-        "${SDL2_MIXER_DIR}/include/SDL2"
-        "${SDL2_MIXER_DIR}/i686-w64-mingw32/include/SDL2"
-        ${PC_SDL2_MIXER_INCLUDE_DIRS})
-endif()
-
-# Find the version.  Taken and modified from CMake's FindSDL.cmake.
-if(SDL2_MIXER_INCLUDE_DIR AND EXISTS "${SDL2_MIXER_INCLUDE_DIR}/SDL_mixer.h")
-    file(STRINGS "${SDL2_MIXER_INCLUDE_DIR}/SDL_mixer.h" SDL2_MIXER_VERSION_MAJOR_LINE REGEX "^#define[ \t]+SDL_MIXER_MAJOR_VERSION[ \t]+[0-9]+$")
-    file(STRINGS "${SDL2_MIXER_INCLUDE_DIR}/SDL_mixer.h" SDL2_MIXER_VERSION_MINOR_LINE REGEX "^#define[ \t]+SDL_MIXER_MINOR_VERSION[ \t]+[0-9]+$")
-    file(STRINGS "${SDL2_MIXER_INCLUDE_DIR}/SDL_mixer.h" SDL2_MIXER_VERSION_PATCH_LINE REGEX "^#define[ \t]+SDL_MIXER_PATCHLEVEL[ \t]+[0-9]+$")
-    string(REGEX REPLACE "^#define[ \t]+SDL_MIXER_MAJOR_VERSION[ \t]+([0-9]+)$" "\\1" SDL2_MIXER_VERSION_MAJOR "${SDL2_MIXER_VERSION_MAJOR_LINE}")
-    string(REGEX REPLACE "^#define[ \t]+SDL_MIXER_MINOR_VERSION[ \t]+([0-9]+)$" "\\1" SDL2_MIXER_VERSION_MINOR "${SDL2_MIXER_VERSION_MINOR_LINE}")
-    string(REGEX REPLACE "^#define[ \t]+SDL_MIXER_PATCHLEVEL[ \t]+([0-9]+)$" "\\1" SDL2_MIXER_VERSION_PATCH "${SDL2_MIXER_VERSION_PATCH_LINE}")
-    set(SDL2_MIXER_VERSION "${SDL2_MIXER_VERSION_MAJOR}.${SDL2_MIXER_VERSION_MINOR}.${SDL2_MIXER_VERSION_PATCH}")
-    unset(SDL2_MIXER_VERSION_MAJOR_LINE)
-    unset(SDL2_MIXER_VERSION_MINOR_LINE)
-    unset(SDL2_MIXER_VERSION_PATCH_LINE)
-    unset(SDL2_MIXER_VERSION_MAJOR)
-    unset(SDL2_MIXER_VERSION_MINOR)
-    unset(SDL2_MIXER_VERSION_PATCH)
-endif()
-
-# Find the library.
-if(CMAKE_SIZEOF_VOID_P STREQUAL 8)
-    find_library(SDL2_MIXER_LIBRARY "SDL2_mixer"
-        HINTS
-        "${SDL2_MIXER_DIR}/lib"
-        "${SDL2_MIXER_DIR}/lib/x64"
-        "${SDL2_MIXER_DIR}/x86_64-w64-mingw32/lib"
-        ${PC_SDL2_MIXER_LIBRARY_DIRS})
-else()
-    find_library(SDL2_MIXER_LIBRARY "SDL2_mixer"
-        HINTS
-        "${SDL2_MIXER_DIR}/lib"
-        "${SDL2_MIXER_DIR}/lib/x86"
-        "${SDL2_MIXER_DIR}/i686-w64-mingw32/lib"
-        ${PC_SDL2_MIXER_LIBRARY_DIRS})
-endif()
-
-include(FindPackageHandleStandardArgs)
-find_package_handle_standard_args(SDL2_mixer
-    FOUND_VAR SDL2_MIXER_FOUND
-    REQUIRED_VARS SDL2_MIXER_INCLUDE_DIR SDL2_MIXER_LIBRARY
-    VERSION_VAR SDL2_MIXER_VERSION
-)
-
-if(SDL2_MIXER_FOUND)
-    # Imported target.
-    add_library(SDL2_mixer::SDL2_mixer UNKNOWN IMPORTED)
-    set_target_properties(SDL2_mixer::SDL2_mixer PROPERTIES
-                          INTERFACE_COMPILE_OPTIONS "${PC_SDL2_MIXER_CFLAGS_OTHER}"
-                          INTERFACE_INCLUDE_DIRECTORIES "${SDL2_MIXER_INCLUDE_DIR}"
-                          INTERFACE_LINK_LIBRARIES SDL2::SDL2
-                          IMPORTED_LOCATION "${SDL2_MIXER_LIBRARY}")
-
-    if(WIN32)
-        # On Windows, we need to figure out the location of our library files
-        # so we can copy and package them.
-        get_filename_component(SDL2_MIXER_DLL_DIR "${SDL2_MIXER_LIBRARY}" DIRECTORY)
-        if(EXISTS "${SDL2_MIXER_DLL_DIR}/SDL2_mixer.dll")
-            set(SDL2_MIXER_DLL_DIR "${SDL2_MIXER_DLL_DIR}" CACHE INTERNAL "")
-        elseif(EXISTS "${SDL2_MIXER_DLL_DIR}/../bin/SDL2_mixer.dll")
-            get_filename_component(SDL2_MIXER_DLL_DIR "${SDL2_MIXER_DLL_DIR}/../bin" REALPATH)
-            set(SDL2_MIXER_DLL_DIR "${SDL2_MIXER_DLL_DIR}" CACHE INTERNAL "")
-        else()
-            message(ERROR "Can't find SDL2_mixer dynamic library directory.")
-        endif()
-    endif()
-endif()

cmake/triplets/x64-windows.cmake
@@ -2,8 +2,3 @@ set(VCPKG_TARGET_ARCHITECTURE x64)
 set(VCPKG_CRT_LINKAGE dynamic)
 set(VCPKG_LIBRARY_LINKAGE dynamic)
 set(VCPKG_BUILD_TYPE release)
-
-if(${PORT} MATCHES "libsamplerate")
-    set(VCPKG_CXX_FLAGS "/fp:fast")
-    set(VCPKG_C_FLAGS "/fp:fast")
-endif()

commit f9861a28618b504a1cd209090439de4eae925e70

fix hang on OPL3 emulation with no sound card
Since Woof! 11.0.0, if the MIDI player is set to OPL3 emulation but sound
initialization fails (such as when there is no sound card available),
Woof hangs. The hang occurs within `OPL_Delay` due to a deadlock: since
the player thread is not running, the delay's callback never runs.

In previous versions, `OPL_SDL_Init` would fail when sound initialization
failed, and it prevented the rest of the OPL initialization logic,
including the call to `OPL_Delay`.
However, this broke when switching from SDL_Mixer to OpenAL.

This commit re-adds the checks so `OPL_SDL_Init` fails when sound
initialization fails.

Fixes: 1a278d6105c8 ("switch to OpenAl for sound mixing (#967)")

opl/opl_sdl.c
@@ -251,7 +251,8 @@ static int OPL_SDL_Init(unsigned int port_base)
     callback_mutex = SDL_CreateMutex();
     callback_queue_mutex = SDL_CreateMutex();
 
-    I_OAL_HookMusic(OPL_Callback);
+    if (!I_OAL_HookMusic(OPL_Callback))
+        return 0;
     I_OAL_SetGain((float)opl_gain / 100.0f);
 
     return 1;

src/i_oalmusic.c
@@ -321,7 +321,7 @@ void I_OAL_SetGain(float gain)
     alSourcef(player.source, AL_GAIN, (ALfloat)gain);
 }
 
-void I_OAL_HookMusic(callback_func_t callback_func)
+boolean I_OAL_HookMusic(callback_func_t callback_func)
 {
     if (callback_func)
     {
@@ -333,12 +333,14 @@ void I_OAL_HookMusic(callback_func_t callback_func)
 
         I_OAL_SetGain(1.0f);
         I_OAL_PlaySong(NULL, false);
+        return player_thread_running;
     }
     else
     {
         I_OAL_UnRegisterSong(NULL);
 
         callback = NULL;
+        return true;
     }
 }
 

src/i_oalmusic.h
@@ -18,10 +18,11 @@
 #define __I_OALMUSIC__
 
 #include <stdint.h>
+#include "doomdef.h"
 
 typedef uint32_t (*callback_func_t)(uint8_t *buffer, uint32_t buffer_samples);
 
-void I_OAL_HookMusic(callback_func_t callback_func);
+boolean I_OAL_HookMusic(callback_func_t callback_func);
 void I_OAL_SetGain(float gain);
 
 #endif