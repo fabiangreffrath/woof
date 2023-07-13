c1602dc7ab097bc182db52b747c0f342c90c80e2
Roman Fomin <rfomin@gmail.com>
2023-07-13 09:08:33 +0700

    fix automap marks in non-follow mode

--- src/am_map.c
+++ src/am_map.c
@@ -380,17 +380,8 @@ static void AM_addMark(void)
                             markpointnum_max*2 : 16) * sizeof(*markpoints),
                            PU_STATIC, 0);
 
-  // [crispy] keep the map static if not following the player
-  if (!followplayer)
-  {
-    markpoints[markpointnum].x = plr->mo->x >> FRACTOMAPBITS;
-    markpoints[markpointnum].y = plr->mo->y >> FRACTOMAPBITS;
-  }
-  else
-  {
   markpoints[markpointnum].x = m_x + m_w/2;
   markpoints[markpointnum].y = m_y + m_h/2;
-  }
   markpointnum++;
 }
 
