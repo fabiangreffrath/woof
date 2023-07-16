87dcd766f3480d3278b20fd57a69b45c4aab2fdb
Fabian Greffrath <fabian@greffrath.com>
2023-07-16 13:49:29 +0200

    allow separate key binding for the numerics keypad
    Fixes #1147

--- src/doomkeys.h
+++ src/doomkeys.h
@@ -24,70 +24,70 @@
 // This is the stuff configured by Setup.Exe.
 // Most key data are simple ascii (uppercased).
 //
-#define KEY_RIGHTARROW  0xae
-#define KEY_LEFTARROW   0xac
-#define KEY_UPARROW     0xad
-#define KEY_DOWNARROW   0xaf
-#define KEY_ESCAPE      27
-#define KEY_ENTER       13
-#define KEY_TAB         9
-#define KEY_F1          (0x80+0x3b)
-#define KEY_F2          (0x80+0x3c)
-#define KEY_F3          (0x80+0x3d)
-#define KEY_F4          (0x80+0x3e)
-#define KEY_F5          (0x80+0x3f)
-#define KEY_F6          (0x80+0x40)
-#define KEY_F7          (0x80+0x41)
-#define KEY_F8          (0x80+0x42)
-#define KEY_F9          (0x80+0x43)
-#define KEY_F10         (0x80+0x44)
-#define KEY_F11         (0x80+0x57)
-#define KEY_F12         (0x80+0x58)
 
-#define KEY_BACKSPACE   0x7f
-#define KEY_PAUSE       0xff
+#define KEY_EQUALS      '='
+#define KEY_MINUS       '-'
 
-#define KEY_EQUALS      0x3d
-#define KEY_MINUS       0x2d
+enum {
 
-#define KEY_RSHIFT      (0x80+0x36)
-#define KEY_RCTRL       (0x80+0x1d)
-#define KEY_RALT        (0x80+0x38)
+    // Keys without character representations
 
-#define KEY_LALT        KEY_RALT
+    KEY_RIGHTARROW = 0x80,
+    KEY_LEFTARROW,
+    KEY_UPARROW,
+    KEY_DOWNARROW,
+    KEY_ESCAPE,
+    KEY_ENTER,
+    KEY_TAB,
+    KEY_F1,
+    KEY_F2,
+    KEY_F3,
+    KEY_F4,
+    KEY_F5,
+    KEY_F6,
+    KEY_F7,
+    KEY_F8,
+    KEY_F9,
+    KEY_F10,
+    KEY_F11,
+    KEY_F12,
+    KEY_BACKSPACE,
+    KEY_PAUSE,
+    KEY_RSHIFT,
+    KEY_RCTRL,
+    KEY_RALT,
+    KEY_LALT = KEY_RALT,
+    KEY_CAPSLOCK,
+    KEY_NUMLOCK,
+    KEY_SCRLCK,
+    KEY_PRTSCR,
+    KEY_HOME,
+    KEY_END,
+    KEY_PGUP,
+    KEY_PGDN,
+    KEY_INS,
+    KEY_DEL,
 
-// new keys:
+    // Keys on the numerics keypad
 
-#define KEY_CAPSLOCK    (0x80+0x3a)
-#define KEY_NUMLOCK     (0x80+0x45)
-#define KEY_SCRLCK      (0x80+0x46)
-#define KEY_PRTSCR      (0x80+0x59)
-
-#define KEY_HOME        (0x80+0x47)
-#define KEY_END         (0x80+0x4f)
-#define KEY_PGUP        (0x80+0x49)
-#define KEY_PGDN        (0x80+0x51)
-#define KEY_INS         (0x80+0x52)
-#define KEY_DEL         (0x80+0x53)
-
-#define KEYP_0          KEY_INS
-#define KEYP_1          KEY_END
-#define KEYP_2          KEY_DOWNARROW
-#define KEYP_3          KEY_PGDN
-#define KEYP_4          KEY_LEFTARROW
-#define KEYP_5          (0x80+0x4c)
-#define KEYP_6          KEY_RIGHTARROW
-#define KEYP_7          KEY_HOME
-#define KEYP_8          KEY_UPARROW
-#define KEYP_9          KEY_PGUP
-
-#define KEYP_DIVIDE     '/'
-#define KEYP_PLUS       '+'
-#define KEYP_MINUS      '-'
-#define KEYP_MULTIPLY   '*'
-#define KEYP_PERIOD     0
-#define KEYP_EQUALS     KEY_EQUALS
-#define KEYP_ENTER      KEY_ENTER
+    KEYP_0,
+    KEYP_1,
+    KEYP_2,
+    KEYP_3,
+    KEYP_4,
+    KEYP_5,
+    KEYP_6,
+    KEYP_7,
+    KEYP_8,
+    KEYP_9,
+    KEYP_DIVIDE,
+    KEYP_PLUS,
+    KEYP_MINUS,
+    KEYP_MULTIPLY,
+    KEYP_PERIOD,
+    KEYP_EQUALS = KEY_EQUALS,
+    KEYP_ENTER = KEY_ENTER,
+};
 
 #define SCANCODE_TO_KEYS_ARRAY {                                            \
     0,   0,   0,   0,   'a',                                  /* 0-9 */     \
