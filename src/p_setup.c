b38afbd5cd4820db284a3eac1c0acc305fae5a5c
Roman Fomin <rfomin@gmail.com>
2023-07-28 13:28:17 +0700

    all sounds are stopped before Z_FreeTags even with MUSINFO

--- src/p_setup.c
+++ src/p_setup.c
@@ -1552,14 +1552,8 @@ void P_SetupLevel(int episode, int map, int playermask, skill_t skill)
     playback_nextlevel = false;
   }
 
-  // [crispy] don't load map's default music if loaded from a savegame with
-  // MUSINFO data
-  if (!musinfo.from_savegame)
-  {
   // Make sure all sounds are stopped before Z_FreeTags.
   S_Start();
-  }
-  musinfo.from_savegame = false;
 
   Z_FreeTag(PU_LEVEL);
   Z_FreeTag(PU_CACHE);
