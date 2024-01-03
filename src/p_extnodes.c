2bbd20a6e3320895e1ebd05ec5b8b4435d36bc7d
Fabian Greffrath <fabian@greffrath.com>
2024-01-03 14:38:50 +0100

    assign sqrt() result to variable, cast and bit-shift this

--- src/p_extnodes.c
+++ src/p_extnodes.c
@@ -195,11 +195,13 @@ mapformat_t P_CheckMapFormat(int lumpnum)
 fixed_t P_GetOffset(vertex_t *v1, vertex_t *v2)
 {
     fixed_t dx, dy;
+    double rf;
     fixed_t r;
 
     dx = (v1->x - v2->x) >> FRACBITS;
     dy = (v1->y - v2->y) >> FRACBITS;
-    r = (fixed_t) (sqrt(dx * dx + dy * dy)) << FRACBITS;
+    rf = sqrt(dx * dx + dy * dy);
+    r = (fixed_t) rf << FRACBITS;
 
     return r;
 }
