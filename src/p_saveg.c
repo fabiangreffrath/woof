467aab5b594bb30dbbe49d134891ecd15e88cb06
Roman Fomin <rfomin@gmail.com>
2024-01-06 08:45:58 +0700

    restore function pointer reading
    There seems to be a check for NULL somewhere.

--- src/p_saveg.c
+++ src/p_saveg.c
@@ -264,8 +264,7 @@ static void saveg_read_thinker_t(thinker_t *str)
     str->next = saveg_readp();
 
     // think_t function;
-    saveg_readp();
-    str->function.v = NULL;
+    str->function.v = (actionf_v)saveg_readp();
 
     // struct thinker_s* cnext;
     str->cnext = saveg_readp();
