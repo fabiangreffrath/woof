1e59d37b9b45efbae9709b84b94715f35566cfbe
ceski <56656010+ceski-1@users.noreply.github.com>
2024-02-14 15:08:57 -0800

    Apply fade to color for stretched skies
    To prevent tiling at a large FOV.

--- src/r_plane.c
+++ src/r_plane.c
@@ -390,6 +390,7 @@ static void do_draw_plane(visplane_t *pl)
 	int texture;
 	angle_t an, flip;
 	boolean vertically_scrolling = false;
+	boolean stretch;
 
 	// killough 10/98: allow skies to come from sidedefs.
 	// Allows scrolling and/or animated skies, as well as
@@ -450,16 +451,19 @@ static void do_draw_plane(visplane_t *pl)
         dc_iscale = skyiscale;
 
         // [FG] stretch short skies
-        if (stretchsky && dc_texheight < 200)
+        stretch = (stretchsky && dc_texheight < 200);
+        if (stretch || !vertically_scrolling)
         {
-          dc_iscale = dc_iscale * dc_texheight / SKYSTRETCH_HEIGHT;
-          dc_texturemid = dc_texturemid * dc_texheight / SKYSTRETCH_HEIGHT;
-          colfunc = R_DrawColumn;
-        }
-        else if (!vertically_scrolling)
-        {
+          fixed_t diff;
+
+          if (stretch)
+          {
+            dc_iscale = dc_iscale * dc_texheight / SKYSTRETCH_HEIGHT;
+            dc_texturemid = dc_texturemid * dc_texheight / SKYSTRETCH_HEIGHT;
+          }
+
           // Make sure the fade-to-color effect doesn't happen too early
-          fixed_t diff = dc_texturemid - SCREENHEIGHT / 2 * FRACUNIT;
+          diff = dc_texturemid - SCREENHEIGHT / 2 * FRACUNIT;
           if (diff < 0)
           {
             diff += textureheight[texture];
