8048019e720bbbd572ad1fb03a5aa0b0f4641958
Fabian Greffrath <fabian@greffrath.com>
2024-02-16 11:25:45 +0100

    update README.md with the newest additions (#1466)
    * update README.md with the newest additions
    
    * further overhaul
    
    * fixes
    
    * bump copyright year for all recent contributors
    
    * add Usage section with Cheat Codes and CLI
    
    * there will be no Woof! 12.1.0 --> 13.0.0
    
    * document CMake requirement
    
    * v_video.c takes code from Eternity Engine -> GPL-3

--- src/v_video.c
+++ src/v_video.c
@@ -1,11 +1,12 @@
 //
 //  Copyright (C) 1999 by
 //  id Software, Chi Hoang, Lee Killough, Jim Flynn, Rand Phares, Ty Halderman
+//  Copyright (C) 2013 James Haley et al.
 //
-//  This program is free software; you can redistribute it and/or
-//  modify it under the terms of the GNU General Public License
-//  as published by the Free Software Foundation; either version 2
-//  of the License, or (at your option) any later version.
+//  This program is free software: you can redistribute it and/or modify
+//  it under the terms of the GNU General Public License as published by
+//  the Free Software Foundation, either version 3 of the License, or
+//  (at your option) any later version.
 //
 //  This program is distributed in the hope that it will be useful,
 //  but WITHOUT ANY WARRANTY; without even the implied warranty of
