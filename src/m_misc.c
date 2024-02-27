1d128eca752f12d0a1f95470572672b6b3f36487
Alaux <73968015+MrAlaux@users.noreply.github.com>
2024-02-27 03:20:31 -0300

    Add missing parenthesis to `hud_type` description (#1538)

--- src/m_misc.c
+++ src/m_misc.c
@@ -2733,7 +2733,7 @@ default_t defaults[] = {
     "hud_type",
     (config_t *) &hud_type, NULL,
     {HUD_TYPE_BOOM}, {HUD_TYPE_CRISPY,NUM_HUD_TYPES-1}, number, ss_stat, wad_no,
-    "Fullscreen HUD (0 = Crispy, 1 = Boom (No Bars), 2 = Boom"
+    "Fullscreen HUD (0 = Crispy, 1 = Boom (No Bars), 2 = Boom)"
   },
 
   // backpack changes thresholds
