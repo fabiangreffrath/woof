e3e8a07514e10fd502f58c751e541358816aee2d
ceski <56656010+ceski-1@users.noreply.github.com>
2024-03-19 22:29:29 -0700

    Use old calculations for widescreen at default FOV
    Partially fixes small differences in wall rendering between Woof 12 and 14.

--- src/r_main.c
+++ src/r_main.c
@@ -274,7 +274,7 @@ static fixed_t centerxfrac_nonwide;
 static void R_InitTextureMapping(void)
 {
   register int i,x;
-  fixed_t slopefrac;
+  fixed_t slopefrac, limit;
   angle_t fov;
   double linearskyfactor;
 
@@ -285,13 +285,20 @@ static void R_InitTextureMapping(void)
   // Calc focallength
   //  so FIELDOFVIEW angles covers SCREENWIDTH.
 
-  if (custom_fov == FOV_DEFAULT && centerxfrac == centerxfrac_nonwide)
+  if (custom_fov == FOV_DEFAULT)
   {
     fov = FIELDOFVIEW;
     slopefrac = finetangent[FINEANGLES / 4 + fov / 2];
     focallength = FixedDiv(centerxfrac_nonwide, slopefrac);
     lightfocallength = centerxfrac_nonwide;
     projection = centerxfrac_nonwide;
+    limit = FRACUNIT * 2;
+
+    if (centerxfrac != centerxfrac_nonwide)
+    {
+      fov = atan((double)centerxfrac / centerxfrac_nonwide) * FINEANGLES / M_PI;
+      slopefrac = finetangent[FINEANGLES / 4 + fov / 2];
+    }
   }
   else
   {
@@ -307,15 +314,16 @@ static void R_InitTextureMapping(void)
     slopefrac = finetangent[FINEANGLES / 4 + fov / 2];
     focallength = FixedDiv(centerxfrac, slopefrac);
     projection = centerxfrac / slope;
+    limit = slopefrac;
   }
 
   for (i=0 ; i<FINEANGLES/2 ; i++)
     {
       int t;
-      if (finetangent[i] > slopefrac)
+      if (finetangent[i] > limit)
         t = -1;
       else
-        if (finetangent[i] < -slopefrac)
+        if (finetangent[i] < -limit)
           t = viewwidth+1;
       else
         {
