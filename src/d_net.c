0a3f0d2dbf1727bf6f301d9f5ad65d54e99064e3
Roman Fomin <rfomin@gmail.com>
2024-05-07 07:43:46 +0700

    fix shorttics config parameter

--- src/d_net.c
+++ src/d_net.c
@@ -171,7 +171,15 @@ static void SaveGameSettings(net_gamesettings_t *settings)
     longtics = (demo_compatibility && M_ParmExists("-longtics")) || mbf21;
 
     settings->lowres_turn = ((M_ParmExists("-record") && !longtics) ||
-                             M_ParmExists("-shorttics") || shorttics);
+
+    //!
+    // @category demo
+    // @help
+    //
+    // Play with low turning resolution to emulate demo recording.
+    //
+
+    M_ParmExists("-shorttics") || shorttics);
 
     settings->demo_version = demo_version;
     G_WriteOptions(settings->options);
@@ -215,22 +223,12 @@ static void InitConnectData(net_connect_data_t *connect_data)
     connect_data->gamemode = gamemode;
     connect_data->gamemission = gamemission;
 
-    if (!shorttics)
-    {
-    //!
-    // @category demo
-    // @help
-    //
-    // Play with low turning resolution to emulate demo recording.
-    //
-        shorttics = M_ParmExists("-shorttics");
-    }
-
     longtics = (demo_compatibility && M_ParmExists("-longtics")) || mbf21;
 
     // Are we recording a demo? Possibly set lowres turn mode
 
-    connect_data->lowres_turn = (M_ParmExists("-record") && !longtics) || shorttics;
+    connect_data->lowres_turn = ((M_ParmExists("-record") && !longtics) ||
+                                 M_ParmExists("-shorttics") || shorttics);
 
     // Read checksums of our WAD directory and dehacked information
 
