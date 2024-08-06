e5a0bf8b0c5afef289bf5593d7b6f63bc58db1ed
ceski <56656010+ceski-1@users.noreply.github.com>
2024-08-05 17:11:44 -0700

    Fix opening/closing a gamepad multiple times
    SDL_CONTROLLERDEVICEREMOVED events send the instance ID, not the index.

--- src/i_input.h
+++ src/i_input.h
@@ -30,7 +30,7 @@ void I_FlushGamepadEvents(void);
 void I_SetSensorEventState(boolean condition);
 void I_InitGamepad(void);
 void I_OpenGamepad(int which);
-void I_CloseGamepad(int which);
+void I_CloseGamepad(SDL_JoystickID instance_id);
 
 void I_ReadMouse(void);
 void I_UpdateGamepad(enum evtype_e type, boolean axis_buttons);
