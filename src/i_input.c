31d7a394907866134b2cd05ddc392232006aa51a
ceski <56656010+ceski-1@users.noreply.github.com>
2024-08-11 12:49:22 -0700

    Enable bluetooth gyro support for DualShock 4 and DualSense

--- src/i_input.c
+++ src/i_input.c
@@ -415,6 +415,10 @@ void I_InitGamepad(void)
         return;
     }
 
+    // Enable bluetooth gyro support for DualShock 4 and DualSense.
+    SDL_SetHint(SDL_HINT_JOYSTICK_HIDAPI_PS4_RUMBLE, "1");
+    SDL_SetHint(SDL_HINT_JOYSTICK_HIDAPI_PS5_RUMBLE, "1");
+
     if (SDL_Init(SDL_INIT_GAMECONTROLLER) < 0)
     {
         I_Printf(VB_WARNING,
