8ef0c811a0ff47730d85c11782654c4a03421ba2
gendlin <108626604+gendlin@users.noreply.github.com>
2024-08-16 09:17:49 -0400

    Add config option to stretch output to fit screen

--- src/i_video.c
+++ src/i_video.c
@@ -92,6 +92,8 @@ static boolean change_display_resolution;
 static int widescreen, default_widescreen;
 static boolean vga_porch_flash; // emulate VGA "porch" behaviour
 static boolean smooth_scaling;
+static boolean full_frame;
+static boolean has_margins;
 static int video_display = 0; // display index
 static boolean disk_icon; // killough 10/98
 
@@ -264,6 +266,8 @@ static void FocusLost(void)
 #define FocusLost()
 #endif
 
+static void UpdateViewport(void);
+
 // [FG] window event handling from Chocolate Doom 3.0
 
 static void HandleWindowEvent(SDL_WindowEvent *event)
@@ -313,6 +317,7 @@ static void HandleWindowEvent(SDL_WindowEvent *event)
                 SDL_GetWindowPosition(screen, &window_x, &window_y);
             }
             window_resize = true;
+            UpdateViewport();
             break;
 
         case SDL_WINDOWEVENT_MOVED:
@@ -413,6 +418,8 @@ static void I_ToggleFullScreen(void)
         SDL_SetWindowResizable(screen, SDL_TRUE);
         SDL_SetWindowSize(screen, window_width, window_height);
     }
+
+    UpdateViewport();
 }
 
 static void I_ToggleExclusiveFullScreen(void)
@@ -662,7 +669,11 @@ static void UpdateRender(void)
     SDL_LowerBlit(screenbuffer, &blit_rect, argbbuffer, &blit_rect);
     SDL_UpdateTexture(texture, &blit_rect, argbbuffer->pixels,
                       argbbuffer->pitch);
-    SDL_RenderClear(renderer);
+
+    if (has_margins)
+    {
+        SDL_RenderClear(renderer);
+    }
 
     if (texture_upscaled)
     {
@@ -1389,15 +1400,52 @@ static void CreateUpscaledTexture(boolean force)
     SDL_SetTextureScaleMode(texture_upscaled, SDL_ScaleModeLinear);
 }
 
+static void UpdateMarginState(void)
+{
+    int w = 1, h = 1;
+    SDL_GetRendererOutputSize(renderer, &w, &h);
+
+    float scalex, scaley;
+    SDL_RenderGetScale(renderer, &scalex, &scaley);
+    SDL_RenderSetScale(renderer, 1.f, 1.f);
+
+    SDL_Rect vp;
+    SDL_RenderGetViewport(renderer, &vp);
+    has_margins = vp.x != 0 || vp.y != 0 || vp.w != w || vp.h != h;
+
+    SDL_RenderSetScale(renderer, scalex, scaley);
+}
+
+static void UpdateViewport(void)
+{
+    int w, h;
+
+    if (full_frame
+        && SDL_GetRendererOutputSize(renderer, &w, &h) == 0
+        && fabs((double)w / h - CurrentAspectRatio()) < .002)
+    {
+        float scalex = (float)w / video.width;
+        float scaley = (float)h / actualheight;
+        SDL_RenderSetLogicalSize(renderer, 0, 0);
+        SDL_RenderSetScale(renderer, scalex, scaley);
+        has_margins = false;
+    }
+    else if (SDL_RenderSetLogicalSize(renderer, video.width, actualheight))
+    {
+        I_Printf(VB_ERROR, "Failed to set logical size: %s", SDL_GetError());
+    }
+    else
+    {
+        UpdateMarginState();
+    }
+}
+
 static void ResetLogicalSize(void)
 {
     blit_rect.w = video.width;
     blit_rect.h = video.height;
 
-    if (SDL_RenderSetLogicalSize(renderer, video.width, actualheight))
-    {
-        I_Printf(VB_ERROR, "Failed to set logical size: %s", SDL_GetError());
-    }
+    UpdateViewport();
 
     if (smooth_scaling)
     {
@@ -1895,6 +1943,7 @@ void I_BindVideoVariables(void)
               wad_no, "Field of view in degrees");
     BIND_NUM_GENERAL(gamma2, 9, 0, 17, "Custom gamma level (0 = -4; 9 = 0; 17 = 4)");
     BIND_BOOL_GENERAL(smooth_scaling, true, "Smooth pixel scaling");
+    BIND_BOOL(full_frame, false, "Stretch image slightly to fit the window");
 
     BIND_BOOL(vga_porch_flash, false, "Emulate VGA \"porch\" behaviour");
     BIND_BOOL(disk_icon, false, "Flashing icon during disk I/O");
