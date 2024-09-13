8c299e0f9345e6b88181bb1406c3dfd9bd4bc146
Jameson Ernst <jameson@jpernst.com>
2024-09-13 12:30:58 -0700

    Use correct OPL3 channel polarity by default

--- src/i_oplmusic.c
+++ src/i_oplmusic.c
@@ -359,7 +359,7 @@ static unsigned int last_perc_count;
 // Configuration file variable, containing the port number for the
 // adlib chip.
 
-static char *snd_dmxoption = "-opl3"; // [crispy] default to OPL3 emulation
+static char *snd_dmxoption = "-opl3-reverse"; // [crispy] default to OPL3 emulation
 static int opl_io_port = 0x388;
 
 // If true, OPL sound channels are reversed to their correct arrangement
