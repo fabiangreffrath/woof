8fc5d4b42a35fd78bdd460b1831ccc523b4637d7
Roman Fomin <rfomin@gmail.com>
2024-11-06 15:39:14 +0700

    fix hud_secret_message binding

--- src/st_widgets.c
+++ src/st_widgets.c
@@ -1088,7 +1088,7 @@ void ST_BindHUDVariables(void)
 
   BIND_BOOL(show_messages, true, "Show messages");
   M_BindNum("hud_secret_message", &hud_secret_message, NULL,
-            SECRETMESSAGE_OFF, SECRETMESSAGE_COUNT, SECRETMESSAGE_ON,
+            SECRETMESSAGE_ON, SECRETMESSAGE_OFF, SECRETMESSAGE_COUNT,
             ss_stat, wad_no,
             "Announce revealed secrets (0 = Off; 1 = On; 2 = Count)");
   M_BindBool("hud_map_announce", &hud_map_announce, NULL,
