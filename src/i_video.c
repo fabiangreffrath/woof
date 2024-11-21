c6fe6d1fc9f842b713f0d6be75d449c88d23499c
gendlin <108626604+gendlin@users.noreply.github.com>
2024-11-21 11:22:22 -0500

    Clean up and match Chocolate

--- src/i_video.c
+++ src/i_video.c
@@ -657,11 +657,13 @@ void I_StartFrame(void)
 
 static void UpdateRender(void)
 {
+    // Blit from the paletted 8-bit screen buffer to the intermediate
+    // 32-bit RGBA buffer and update the intermediate texture with the
+    // contents of the RGBA buffer.
+
     SDL_LockTexture(texture, &blit_rect, &argbbuffer->pixels,
-        &argbbuffer->pitch);
-
+                    &argbbuffer->pitch);
     SDL_LowerBlit(screenbuffer, &blit_rect, argbbuffer, &blit_rect);
-
     SDL_UnlockTexture(texture);
 
     SDL_RenderClear(renderer);
@@ -1767,7 +1769,7 @@ static void CreateSurfaces(int w, int h)
     // [FG] create intermediate ARGB frame buffer
 
     argbbuffer = SDL_CreateRGBSurfaceWithFormatFrom(
-        NULL, w, h, 8, 0, SDL_PIXELFORMAT_ARGB8888);
+        NULL, w, h, 0, 0, SDL_PIXELFORMAT_ARGB8888);
 
     I_SetPalette(W_CacheLumpName("PLAYPAL", PU_CACHE));
 
