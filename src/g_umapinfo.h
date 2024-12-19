655faf3b37a676cf201f93f933583aca62fda906
Roman Fomin <rfomin@gmail.com>
2024-12-19 19:44:02 +0700

    update copyright and README.md

--- src/g_umapinfo.h
+++ src/g_umapinfo.h
@@ -1,22 +1,16 @@
-//-----------------------------------------------------------------------------
 //
-// Copyright 2017 Christoph Oelckers
-// Copyright 2019 Fernando Carmona Varo
+//  Copyright(C) 2017 Christoph Oelckers
+//  Copyright(C) 2021 Roman Fomin
 //
-// This program is free software: you can redistribute it and/or modify
-// it under the terms of the GNU Lesser General Public License as published by
-// the Free Software Foundation, either version 2 of the License, or
-// (at your option) any later version.
+//  This program is free software; you can redistribute it and/or
+//  modify it under the terms of the GNU General Public License
+//  as published by the Free Software Foundation; either version 2
+//  of the License, or (at your option) any later version.
 //
-// This program is distributed in the hope that it will be useful,
-// but WITHOUT ANY WARRANTY; without even the implied warranty of
-// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-// GNU General Public License for more details.
-//
-// You should have received a copy of the GNU General Public License
-// along with this program.  If not, see http://www.gnu.org/licenses/
-//
-//-----------------------------------------------------------------------------
+//  This program is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU General Public License for more details.
 
 #ifndef G_UMAPINFO_H
 #define G_UMAPINFO_H
