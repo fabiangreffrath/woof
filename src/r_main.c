344d86d61b6387a9f23e8bfae468972fa983c41b
Roman Fomin <rfomin@gmail.com>
2025-01-28 08:24:00 +0700

    fix linearsky calculation for large sky texture width (#2160)

--- src/r_main.c
+++ src/r_main.c
@@ -367,10 +367,7 @@ static void R_InitTextureMapping(void)
       xtoviewangle[x] = (i<<ANGLETOFINESHIFT)-ANG90;
       // [FG] linear horizontal sky scrolling
       int angle = (0.5 - x / (double)viewwidth) * linearskyfactor;
-      if (angle >= 0)
-        linearskyangle[x] = angle;
-      else
-        linearskyangle[x] = ANG90 + angle;
+      linearskyangle[x] = (angle >= 0) ? angle : ANGLE_MAX + angle;
     }
     
   // Take out the fencepost cases from viewangletox.
