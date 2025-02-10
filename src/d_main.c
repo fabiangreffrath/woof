1e5f4c8db4767b344b32afbff6cb594cb9007417
elf-alchemist <alchemist.software@proton.me>
2025-02-10 12:21:40 -0300

    Add NyanDoom widescreen patch support (#2191)

--- src/d_main.c
+++ src/d_main.c
@@ -460,7 +460,8 @@ void D_PageTicker(void)
 
 void D_PageDrawer(void)
 {
-  V_DrawPatchFullScreen(V_CachePatchName(pagename, PU_CACHE));
+  V_DrawPatchFullScreen(
+    V_CachePatchName(W_CheckWidescreenPatch(pagename), PU_CACHE));
 }
 
 //
