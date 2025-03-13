07e6d4206734a48ce97d8a3ce59a3d2cdbd03535
Fabian Greffrath <fabian@greffrath.com>
2025-03-13 22:26:18 +0100

    comment on PL_FLATMAPPING composition

--- src/r_sky.h
+++ src/r_sky.h
@@ -39,7 +39,7 @@ extern boolean linearsky;
 extern int skytexture;
 extern int skytexturemid;
 
-#define PL_FLATMAPPING (0xC0000000)
+#define PL_FLATMAPPING (0xC0000000) // (PL_SKYFLAT | 0x40000000)
 
 extern sky_t *sky;
 
