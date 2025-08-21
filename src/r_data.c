53a73c206b0b3864cfcc130f3807ec8bf735addf
Guilherme M. Miranda <alchemist.software@proton.me>
2025-08-21 13:10:00 -0300

    cleanup

--- src/r_data.c
+++ src/r_data.c
@@ -1143,7 +1143,11 @@ int R_FlatNumForName(const char *name)    // killough -- const added
   if (i == NO_TEXTURE)
   {
     I_Printf(VB_WARNING, "R_FlatNumForName: %.8s not found", name);
-    return R_FlatNumForName("-NO_TEX-"); // highlight missing flats
+    i = (W_CheckNumForName)("-NO_TEX-", ns_flats); // highlight missing flats
+    if (i == NO_TEXTURE)
+    {
+      I_Error("Could not find '-NO_TEX-' lump");
+    }
   }
   return i - firstflat;
 }
