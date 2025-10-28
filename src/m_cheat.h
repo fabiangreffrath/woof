1e6e1035bf68aa384dbfb306bee25a481e0574fc
Roman Fomin <rfomin@gmail.com>
2025-10-28 11:45:49 +0700

    rewrite M_FindCheats (from Chocolate Doom), add "The Sky May Be" to compdb

--- src/m_cheat.h
+++ src/m_cheat.h
@@ -48,14 +48,20 @@ typedef enum {
   not_net = not_dm | not_coop
 } cheat_when_t;
 
+#define CHEAT_ARGS_MAX 8
+
 extern struct cheat_s {
   const char *cheat; // [FG] char!
   const char *const deh_cheat;
   const cheat_when_t when;
   const cheatf_t func;
   const int arg;
-  uint64_t code, mask;
+  boolean repeatable;
   boolean deh_modified;                // killough 9/12/98
+
+  int sequence_len;
+  int chars_read, param_chars_read;
+  char parameter_buf[CHEAT_ARGS_MAX];
 } cheat[];
 
 void cheat_mypos_print();
