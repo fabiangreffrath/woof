246ea42591a622e04830995e2273d69ed431040f
Fabian Greffrath <fabian@greffrath.com>
2026-01-03 10:57:16 +0100

    let IDDKT cheat skip spawned monsters
    https://github.com/kraflab/dsda-doom/commit/a27b84fb831b29dfe974318d23a3acd4cff1a7ef

--- src/m_cheat.c
+++ src/m_cheat.c
@@ -1055,6 +1055,11 @@ static void cheat_cycle_mobj(mobj_t **last_mobj, int *last_count,
 
       mobj = (mobj_t *) th;
 
+      if (mobj->intflags & MIF_SPAWNED_BY_ICON)
+      {
+        continue;
+      }
+
       if ((!alive || mobj->health > 0) && mobj->flags & flags)
       {
         followplayer = false;
