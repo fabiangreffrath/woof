eb817631a44efa77979bade3247f08cc08aacb4d
Roman Fomin <rfomin@gmail.com>
2026-01-10 11:21:50 +0700

    implementation of map line thickness, amapdef.json and KEX arrow (#2540)
    * rewrite implementation of Xiaolin Wu's algorithm using floats
    
    * add "auto" option
    
    * implement amapdef.json, player arrow from KEX port
    
    * cosmetic changes

--- src/mn_setup.c
+++ src/mn_setup.c
@@ -362,6 +362,7 @@ enum
     str_hudcolor,
     str_secretmessage,
     str_overlay,
+    str_automap_thickness,
     str_automap_preset,
     str_automap_keyed_door,
     str_fuzzmode,
@@ -2075,6 +2076,9 @@ void MN_DrawStatusHUD(void)
 
 static const char *overlay_strings[] = {"Off", "On", "Dark"};
 
+static const char *automap_thickness_strings[] = {
+    "Auto", "1", "2", "3", "4", "5", "6"};
+
 static const char *automap_preset_strings[] = {"Vanilla", "Crispy", "Boom", "ZDoom"};
 
 static const char *automap_keyed_door_strings[] = {"Off", "On", "Flashing"};
@@ -2087,6 +2091,12 @@ static setup_menu_t auto_settings1[] = {
     {"Rotate Automap",  S_ONOFF,  H_X, M_SPC, {"automaprotate"}},
     {"Overlay Automap", S_CHOICE, H_X, M_SPC, {"automapoverlay"},
      .strings_id = str_overlay},
+
+    MI_GAP,
+
+    {"Line Thickness", S_THERMO | S_THRM_SIZE4, H_X, M_THRM_SPC,
+     {"map_line_thickness"}, .strings_id = str_automap_thickness,
+     .action = AM_ResetThickness},
 
     MI_GAP,
 
@@ -5003,6 +5013,7 @@ static const char **selectstrings[] = {
     [str_hudcolor] = hudcolor_strings,
     [str_secretmessage] = secretmessage_strings,
     [str_overlay] = overlay_strings,
+    [str_automap_thickness] = automap_thickness_strings,
     [str_automap_preset] = automap_preset_strings,
     [str_automap_keyed_door] = automap_keyed_door_strings,
     [str_fuzzmode] = fuzzmode_strings,
