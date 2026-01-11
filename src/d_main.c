42cc6ff9dc0d8331b39dc68fcc679c6722bd3646
Guilherme M. Miranda <alchemist.software@proton.me>
2026-01-11 11:33:04 -0300

    fix -warp wipe

--- src/d_main.c
+++ src/d_main.c
@@ -222,7 +222,7 @@ void D_ProcessEvents (void)
 
 // wipegamestate can be set to -1 to force a wipe on the next draw
 gamestate_t wipegamestate = GS_DEMOSCREEN;
-wipefx_t    screen_wipe_internal = wipe_None;
+wipefx_t    screen_wipe_internal = wipe_Melt;
 wipefx_t    screen_wipe = wipe_None;
 
 void D_Display (void)
