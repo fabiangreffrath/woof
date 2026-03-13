5e93a76b2bb6199952c4e68aaff4e127beee8be6
Roman Fomin <rfomin@gmail.com>
2026-03-13 17:00:02 +0700

    vendor libebur128, make libsndfile optional (#2635)
    * add support of libspng from vcpkg
    
    * add fix for libspng config

--- src/i_oalsound.c
+++ src/i_oalsound.c
@@ -682,6 +682,7 @@ boolean I_OAL_CacheSound(sfxinfo_t *sfx)
             // All Doom sounds are 8-bit
             format = AL_FORMAT_MONO8;
         }
+#ifdef HAVE_SNDFILE
         else
         {
             size = lumplen;
@@ -696,12 +697,17 @@ boolean I_OAL_CacheSound(sfxinfo_t *sfx)
 
             sampledata = wavdata;
         }
-
-        alGetError();
+#else
+        else
+        {
+            I_Printf(VB_ERROR, " I_OAL_CacheSound: %s", lumpinfo[lumpnum].name);
+            break;
+        }
+#endif
         alGenBuffers(1, &buffer);
         if (alGetError() != AL_NO_ERROR)
         {
-            I_Printf(VB_ERROR, "I_OAL_CacheSound: Error creating buffers.");
+            I_Printf(VB_ERROR, "I_OAL_CacheSound: Error creating buffer.");
             break;
         }
         alBufferData(buffer, format, sampledata, size, freq);
