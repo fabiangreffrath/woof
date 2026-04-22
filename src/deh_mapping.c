f4d31d4105769cc633e27ba034f0043a23c17262
Fabian Greffrath <fabian@greffrath.com>
2026-04-22 13:19:40 +0200

    fix DEH string mapping (#2665)
    * fix DEH string mapping
    
    The previous code copied (parts of) a string to a pointer.
    
    Fixes #2663
    
    * free *location if already non-NULL
    
    * refactor
    
    * strip unnecessary parameter

--- src/deh_mapping.c
+++ src/deh_mapping.c
@@ -18,6 +18,7 @@
 // name
 //
 
+#include <stdlib.h>
 #include "deh_mapping.h"
 #include "deh_io.h"
 #include "doomtype.h"
@@ -57,10 +58,28 @@ static void *GetStructField(void *structptr, deh_mapping_t *mapping, deh_mapping
 }
 
 //
+// Set the value of a string field in a structure by name
+//
+
+static boolean SetStringMapping(deh_mapping_t *mapping, deh_mapping_entry_t *entry,
+                                void *structptr, char *value)
+{
+    char **location = GetStructField(structptr, mapping, entry);
+    // Copy value into field:
+    if (*location)
+    {
+        free(*location);
+    }
+    *location = M_StringDuplicate(value);
+    return true;
+}
+
+//
 // Set the value of a particular field in a structure by name
 //
 
-boolean DEH_SetMapping(deh_context_t *context, deh_mapping_t *mapping, void *structptr, char *name, int value)
+boolean DEH_SetMapping(deh_context_t *context, deh_mapping_t *mapping,
+                       void *structptr, char *name, int ivalue, char *value)
 {
     deh_mapping_entry_t *entry = GetMappingEntryByName(context, mapping, name);
 
@@ -69,16 +88,14 @@ boolean DEH_SetMapping(deh_context_t *context, deh_mapping_t *mapping, void *str
         return false;
     }
 
-    // Sanity check:
     if (entry->is_string)
     {
-        DEH_Error(context, "Tried to set '%s' as integer (BUG)", name);
-        return false;
+        return SetStringMapping(mapping, entry, structptr, value);
     }
 
     if (entry->translate)
     {
-        value = entry->translate(value);
+        ivalue = entry->translate(ivalue);
     }
 
     void *location = GetStructField(structptr, mapping, entry);
@@ -89,44 +106,19 @@ boolean DEH_SetMapping(deh_context_t *context, deh_mapping_t *mapping, void *str
     switch (entry->size)
     {
         case 1:
-            *((uint8_t *)location) = value;
+            *((uint8_t *)location) = ivalue;
             break;
         case 2:
-            *((uint16_t *)location) = value;
+            *((uint16_t *)location) = ivalue;
             break;
         case 4:
-            *((uint32_t *)location) = value;
+            *((uint32_t *)location) = ivalue;
             break;
         default:
             DEH_Error(context, "Unknown field type for '%s' (BUG)", name);
             return false;
     }
 
-    return true;
-}
-
-//
-// Set the value of a string field in a structure by name
-//
-
-boolean DEH_SetStringMapping(deh_context_t *context, deh_mapping_t *mapping, void *structptr, char *name, char *value)
-{
-    deh_mapping_entry_t *entry = GetMappingEntryByName(context, mapping, name);
-    if (entry == NULL)
-    {
-        return false;
-    }
-
-    // Sanity check:
-    if (!entry->is_string)
-    {
-        DEH_Error(context, "Tried to set '%s' as string (BUG)", name);
-        return false;
-    }
-
-    void *location = GetStructField(structptr, mapping, entry);
-    // Copy value into field:
-    M_StringCopy(location, value, entry->size);
     return true;
 }
 
