6cb17c23167ea6716124241287780b3c6b4816ff
Alaux <73968015+MrAlaux@users.noreply.github.com>
2026-08-17 20:07:39 -0300

    Enable `-funroll_loops`
    By @fabiangreffrath.
    
    Co-Authored-By: Fabian Greffrath <fabian@greffrath.com>

--- cmake/modules/WoofSettings.cmake
+++ cmake/modules/WoofSettings.cmake
@@ -54,6 +54,7 @@ if(MSVC)
     # the compiler to convert the signed value to unsigned.
     _checked_add_compile_option(/wd4018)
 else()
+    _checked_add_compile_option(-funroll-loops)
     _checked_add_compile_option(-Werror=array-bounds)
     _checked_add_compile_option(-Werror=clobbered)
     _checked_add_compile_option(-Werror=format-security)
