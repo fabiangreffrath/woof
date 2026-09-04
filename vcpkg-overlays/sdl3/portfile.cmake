1698a8263f9b3091cfbe0efd2f20a0023c71b80c
Mia McMahill <miadawnmc@gmail.com>
2026-09-04 14:22:28 -0500

    Get rid of reference to usage file

--- vcpkg-overlays/sdl3/portfile.cmake
+++ vcpkg-overlays/sdl3/portfile.cmake
@@ -84,7 +84,6 @@ file(REMOVE_RECURSE
 vcpkg_copy_pdbs()
 vcpkg_fixup_pkgconfig()
 
-file(INSTALL "${CMAKE_CURRENT_LIST_DIR}/usage" DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}")
 vcpkg_install_copyright(
     FILE_LIST
         "${SOURCE_PATH}/LICENSE.txt"
