7d6e6db01a5e837e04bd4fa655b5a9012a72678a
Guilherme M. Miranda <alchemist.software@proton.me>
2026-09-12 23:19:52 -0300

    fix internal flag check

--- src/p_ambient.c
+++ src/p_ambient.c
@@ -50,7 +50,7 @@ static float GetEffectiveOffset(ambient_t *ambient)
 
 void P_GetAmbientSoundParams(ambient_t *ambient, sfxparams_t *params)
 {
-    if (ambient->source->intflags && MIF_PARAMSOUND)
+    if (ambient->source->intflags & MIF_PARAMSOUND)
     {
         int volume_scale = 127;
         int close_dist = S_CLOSE_DIST;
