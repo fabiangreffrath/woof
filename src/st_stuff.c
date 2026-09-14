fabiangreffrath/woof

commit 68ddee02e2cfd4015af20cf36527fca262c1ac4e

draw status bar border for all non-fullscreen HUDs (#2824)
* draw status bar border for all non-fullscreen HUDs

Fixes #2813

* fix viewport size and crosshair position

* generalize further

* no need to call ST_SetSTHeight() in HU_UpdateCrosshair()

* further generalization

src/st_stuff.c
@@ -2212,7 +2212,7 @@ static void DrawStatusBar(void)
 {
     ST_SetSTHeight();
 
-    if (st_height && (screenblocks <= 10 || automap_on))
+    if (st_height && (screenblocks < 10 || !statusbar->fullscreenrender || automap_on))
     {
         DrawBackground(statusbar->fillflat);
     }
@@ -2229,7 +2229,7 @@ static void DrawStatusBar(void)
 
 void ST_Erase(void)
 {
-    if (!sbardef || screenblocks >= 10)
+    if (!sbardef || (screenblocks >= 10 && statusbar->fullscreenrender))
     {
         return;
     }

src/hu_crosshair.c
@@ -127,8 +127,7 @@ void HU_UpdateCrosshair(void)
     plr = &players[displayplayer];
 
     crosshair.x = SCREENWIDTH / 2;
-    crosshair.y = (screenblocks <= 10) ? (SCREENHEIGHT - st_height) / 2
-                                       : SCREENHEIGHT / 2;
+    crosshair.y = (SCREENHEIGHT - st_height) / 2;
 
     boolean invul = ST_PlayerInvulnerable(plr);
 

src/r_main.c
@@ -524,26 +524,18 @@ void R_ExecuteSetViewSize (void)
 
   setsizeneeded = false;
 
-  if (setblocks <= 10)
-  {
-    st_height = st_height_screenblocks10;
-  }
+  if (setblocks >= 10)
+    {
+      ST_SetSTHeight();
 
-  if (setblocks == 11)
-    {
       scaledviewwidth_nonwide = NONWIDEWIDTH;
       scaledviewwidth = video.unscaledw;
-      scaledviewheight = SCREENHEIGHT;                    // killough 11/98
-    }
-  // [crispy] hard-code to SCREENWIDTH and SCREENHEIGHT minus status bar height
-  else if (setblocks == 10)
-    {
-      scaledviewwidth_nonwide = NONWIDEWIDTH;
-      scaledviewwidth = video.unscaledw;
-      scaledviewheight = SCREENHEIGHT - st_height;
+      scaledviewheight = SCREENHEIGHT - st_height; // killough 11/98
     }
   else
     {
+      st_height = st_height_screenblocks10;
+
       const int st_screen = SCREENHEIGHT - st_height;
 
       scaledviewwidth_nonwide = setblocks * 32;

commit 53f91c106d5c2acd76343c38913c1ab9f73a86a7

fix regression introduced by 68ddee0, add changelog entry

src/st_stuff.c
@@ -2202,6 +2202,10 @@ void ST_SetSTHeight(void)
     {
         st_height = CLAMP(statusbar->height, 0, SCREENHEIGHT) & ~1;
     }
+    else if (screenblocks == 10)
+    {
+        st_height = st_height_screenblocks10;
+    }
     else
     {
         st_height = 0;