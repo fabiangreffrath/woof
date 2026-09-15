d5d8ecf37ed4cca63300d29e6d0a4e9eb765530f
Fabian Greffrath <fabian@greffrath.com>
2026-09-15 18:24:32 +0200

    properly fix UB in `R_PointOnSide_*()` (#2877)
    Without relying on `volatile` this time. Fix formatting while at it.

--- src/r_main.c
+++ src/r_main.c
@@ -132,48 +132,53 @@ void (*colfunc)(void);                    // current column draw function
 //
 int (*R_PointOnSide)(fixed_t x, fixed_t y, struct node_s *node) = R_PointOnSide_Classic;
 
-// Workaround for optimization bug in clang
-// fixes desync in competn/doom/fp2-3655.lmp and in dmnsns.wad dmn01m909.lmp
-#if defined(__clang__)
-int R_PointOnSide_Classic(volatile fixed_t x, volatile fixed_t y, node_t *node)
-#else
 int R_PointOnSide_Classic(fixed_t x, fixed_t y, node_t *node)
-#endif
 {
-  if (!node->dx)
-    return x <= node->x ? node->dy > 0 : node->dy < 0;
+    if (!node->dx)
+    {
+        return x <= node->x ? node->dy > 0 : node->dy < 0;
+    }
 
-  if (!node->dy)
-    return y <= node->y ? node->dx < 0 : node->dx > 0;
-        
-  x -= node->x;
-  y -= node->y;
-  
-  // Try to quickly decide by looking at sign bits.
-  if ((node->dy ^ node->dx ^ x ^ y) < 0)
-    return (node->dy ^ x) < 0;  // (left is negative)
-  return FixedMul(y, node->dx>>FRACBITS) >= FixedMul(node->dy>>FRACBITS, x);
+    if (!node->dy)
+    {
+        return y <= node->y ? node->dx < 0 : node->dx > 0;
+    }
+
+    // Workaround for optimization bug in clang
+    // fixes desync in competn/doom/fp2-3655.lmp and in dmnsns.wad dmn01m909.lmp
+    x = (fixed_t)((unsigned int)x - (unsigned int)node->x);
+    y = (fixed_t)((unsigned int)y - (unsigned int)node->y);
+
+    // Try to quickly decide by looking at sign bits.
+    if ((node->dy ^ node->dx ^ x ^ y) < 0)
+    {
+        return (node->dy ^ x) < 0; // (left is negative)
+    }
+    return FixedMul(y, node->dx >> FRACBITS)
+           >= FixedMul(node->dy >> FRACBITS, x);
 }
 
-#if defined(__clang__)
-int R_PointOnSide_Precise(volatile fixed_t x, volatile fixed_t y, node_t *node)
-#else
 int R_PointOnSide_Precise(fixed_t x, fixed_t y, node_t *node)
-#endif
 {
-   if(!node->dx)
-      return x <= node->x ? node->dy > 0 : node->dy < 0;
+    if (!node->dx)
+    {
+        return x <= node->x ? node->dy > 0 : node->dy < 0;
+    }
 
-   if(!node->dy)
-      return y <= node->y ? node->dx < 0 : node->dx > 0;
+    if (!node->dy)
+    {
+        return y <= node->y ? node->dx < 0 : node->dx > 0;
+    }
 
-   x -= node->x;
-   y -= node->y;
+    x = (fixed_t)((unsigned int)x - (unsigned int)node->x);
+    y = (fixed_t)((unsigned int)y - (unsigned int)node->y);
 
-   // Try to quickly decide by looking at sign bits.
-   if((node->dy ^ node->dx ^ x ^ y) < 0)
-      return (node->dy ^ x) < 0;  // (left is negative)
-   return (int64_t)y * node->dx >= (int64_t)node->dy * x;
+    // Try to quickly decide by looking at sign bits.
+    if ((node->dy ^ node->dx ^ x ^ y) < 0)
+    {
+        return (node->dy ^ x) < 0; // (left is negative)
+    }
+    return (int64_t)y * node->dx >= (int64_t)node->dy * x;
 }
 
 // killough 5/2/98: reformatted
