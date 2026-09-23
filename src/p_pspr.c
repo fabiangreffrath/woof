a451a0b469b509d5b24522c3c8cffb8d3e1e7d56
Guilherme M. Miranda <alchemist.software@proton.me>
2026-09-23 15:43:44 -0300

    reformat P_SetPspritePtr while at it

--- src/p_pspr.c
+++ src/p_pspr.c
@@ -100,48 +100,51 @@ static void P_SetPsprite(player_t *player, int position, statenum_t stnum)
 
 void P_SetPspritePtr(player_t *player, pspdef_t *psp, statenum_t stnum)
 {
-  do
+    do
     {
-      state_t *state;
+        state_t *state;
 
-      if (!stnum)
+        if (!stnum)
         {
-          // object removed itself
-          psp->state = NULL;
-          break;
-        }
-
-      // killough 7/19/98: Pre-Beta BFG
-      if (stnum == S_BFG1 && (STRICTMODE(classic_bfg) || beta_emulation))
-	stnum = S_OLDBFG1;                 // Skip to alternative weapon frame
-
-      state = &states[stnum];
-      psp->state = state;
-      psp->tics = state->tics;        // could be 0
-
-      if (state->misc1)
-        {
-          // coordinate set
-          psp->sx = state->misc1 << FRACBITS;
-          psp->sy = state->misc2 << FRACBITS;
-          // [FG] centered weapon sprite
-          psp->sx2 = psp->sx;
-          psp->sy2 = psp->sy;
-          psp->sxf = psp->sx2;
-          psp->syf = psp->sy2;
-        }
-
-      // Call action routine.
-      // Modified handling.
-      if (state->action)
-        {
-          state->action(player->mo, psp);
-          if (!psp->state)
+            // object removed itself
+            psp->state = NULL;
             break;
         }
-      stnum = psp->state->nextstate;
-    }
-  while (!psp->tics);     // an initial state of 0 could cycle through
+
+        // killough 7/19/98: Pre-Beta BFG
+        if (stnum == S_BFG1 && (STRICTMODE(classic_bfg) || beta_emulation))
+        {
+            stnum = S_OLDBFG1; // Skip to alternative weapon frame
+        }
+
+        state = &states[stnum];
+        psp->state = state;
+        psp->tics = state->tics; // could be 0
+
+        if (state->misc1)
+        {
+            // coordinate set
+            psp->sx = state->misc1 << FRACBITS;
+            psp->sy = state->misc2 << FRACBITS;
+            // [FG] centered weapon sprite
+            psp->sx2 = psp->sx;
+            psp->sy2 = psp->sy;
+            psp->sxf = psp->sx2;
+            psp->syf = psp->sy2;
+        }
+
+        // Call action routine.
+        // Modified handling.
+        if (state->action)
+        {
+            state->action(player->mo, psp);
+            if (!psp->state)
+            {
+                break;
+            }
+        }
+        stnum = psp->state->nextstate;
+    } while (!psp->tics); // an initial state of 0 could cycle through
 }
 
 //
